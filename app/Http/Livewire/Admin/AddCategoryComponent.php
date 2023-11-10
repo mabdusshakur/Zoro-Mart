f90796d2dd7d03e8e15c042608e76da71cdb4070
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-11-10 11:32:30 +0600

    working add category added

--- app/Http/Livewire/Admin/AddCategoryComponent.php
+++ app/Http/Livewire/Admin/AddCategoryComponent.php
@@ -3,9 +3,24 @@
 namespace App\Http\Livewire\Admin;
 
 use Livewire\Component;
+use App\Models\Category;
+use Illuminate\Support\Str;
 
 class AddCategoryComponent extends Component
 {
+    public $category_name;
+    public function add_category()
+    {
+        try {
+            $category = new Category();
+            $category->name = $this->category_name;
+            $category->slug = Str::slug($this->category_name);
+            $category->save();
+            session()->flash('success', 'Category has been created successfully!');
+        } catch (\Throwable $th) {
+            session()->flash('error', 'Something went wrong!');
+        }
+    }
     public function render()
     {
         return view('livewire.admin.add-category-component')->layout('layouts.admin');
---
+++ resources/views/partials/admin/_alerts.blade.php
@@ -0,0 +1,51 @@
+@if (Session::has('info'))
+    <div class="alert alert-dismissible fade show py-2 bg-info">
+        <div class="d-flex align-items-center">
+            <div class="fs-3 text-dark"><ion-icon name="information-circle-sharp"></ion-icon>
+            </div>
+            <div class="ms-3">
+                <div class="text-dark">{{ Session::get('info') }}</div>
+            </div>
+        </div>
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+@endif
+
+@if (Session::has('warning'))
+    <div class="alert alert-dismissible fade show py-2 bg-warning">
+        <div class="d-flex align-items-center">
+            <div class="fs-3 text-dark"><ion-icon name="warning-sharp"></ion-icon>
+            </div>
+            <div class="ms-3">
+                <div class="text-dark">{{ Session::get('warning') }}</div>
+            </div>
+        </div>
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+@endif
+
+@if (Session::has('danger'))
+    <div class="alert alert-dismissible fade show py-2 bg-danger">
+        <div class="d-flex align-items-center">
+            <div class="fs-3 text-white"><ion-icon name="close-circle-sharp"></ion-icon>
+            </div>
+            <div class="ms-3">
+                <div class="text-white">{{ Session::get('danger') }}</div>
+            </div>
+        </div>
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+@endif
+
+@if (Session::has('success'))
+    <div class="alert alert-dismissible fade show py-2 bg-success">
+        <div class="d-flex align-items-center">
+            <div class="fs-3 text-white"><ion-icon name="checkmark-circle-sharp"></ion-icon>
+            </div>
+            <div class="ms-3">
+                <div class="text-white">{{ Session::get('success') }}</div>
+            </div>
+        </div>
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+@endif
