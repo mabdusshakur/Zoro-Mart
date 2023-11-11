43744ebd41b43638b3d038a940cd2b574a06a433
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-11-11 21:22:03 +0600

    added all sub_category view working

--- app/Http/Livewire/Admin/SubCategoriesComponent.php
+++ app/Http/Livewire/Admin/SubCategoriesComponent.php
@@ -2,16 +2,26 @@
 
 namespace App\Http\Livewire\Admin;
 
+use App\Models\SubCategory;
 use Livewire\Component;
 
 class SubCategoriesComponent extends Component
 {
+    public function view_sub_category($id)
+    {
+        
+    }
     public function edit_sub_category($id)
     {
         return redirect()->route('admin.edit-sub-category', ['id' => $id]);
     }
+    public function delete_sub_category($id)
+    {
+        
+    }
     public function render()
     {
-        return view('livewire.admin.sub-categories-component')->layout('layouts.admin');
+        $sub_categories = SubCategory::all();
+        return view('livewire.admin.sub-categories-component',['sub_categories' => $sub_categories])->layout('layouts.admin');
     }
 }
--- resources/views/livewire/admin/sub-categories-component.blade.php
+++ resources/views/livewire/admin/sub-categories-component.blade.php
@@ -4,7 +4,8 @@
             <div class="d-flex align-items-center">
                 <h5 class="mb-0">All Sub Categories</h5>
                 <form class="ms-auto position-relative">
-                    <div class="position-absolute top-50 translate-middle-y search-icon px-3"><i class="fa-solid fa-magnifying-glass"></i></div>
+                    <div class="position-absolute top-50 translate-middle-y search-icon px-3"><i
+                            class="fa-solid fa-magnifying-glass"></i></div>
                     <input class="form-control ps-5" type="text" placeholder="search">
                 </form>
             </div>
@@ -19,18 +20,29 @@
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>Sport Shoes</td>
-                            <td>Shoe</td>
-                            <td>56346</td>
-                            <td>
-                                <div class="table-actions d-flex align-items-center gap-3 fs-6">
-                                    <a href="javascript:;" class="text-primary" data-bs-toggle="tooltip" data-bs-placement="bottom" title="" data-bs-original-title="Views" aria-label="Views"><i class="fa-solid fa-eye"></i></a>
-                                    <a href="javascript:;" wire:click="edit_sub_category(1)" class="text-warning" data-bs-toggle="tooltip" data-bs-placement="bottom" title="" data-bs-original-title="Edit" aria-label="Edit"><i class="fa-solid fa-pencil"></i></a>
-                                    <a href="javascript:;" class="text-danger" data-bs-toggle="tooltip" data-bs-placement="bottom" title="" data-bs-original-title="Delete" aria-label="Delete"><i class="fa-solid fa-trash"></i></a>
-                                </div>
-                            </td>
-                        </tr>
+                        @foreach ($sub_categories as $sub_category)
+                            <tr>
+                                <td>{{$sub_category->name}}</td>
+                                <td>{{$sub_category->category_count}}</td>
+                                <td>{{$sub_category->product_count}}</td>
+                                <td>
+                                    <div class="table-actions d-flex align-items-center gap-3 fs-6">
+                                        <a href="javascript:;" wire:click="view_sub_category({{$sub_category->id}})" class="text-primary" data-bs-toggle="tooltip"
+                                            data-bs-placement="bottom" title="" data-bs-original-title="Views"
+                                            aria-label="Views"><i class="fa-solid fa-eye"></i></a>
+
+                                        <a href="javascript:;" wire:click="edit_sub_category({{$sub_category->id}})" class="text-warning"
+                                            data-bs-toggle="tooltip" data-bs-placement="bottom" title=""
+                                            data-bs-original-title="Edit" aria-label="Edit"><i
+                                                class="fa-solid fa-pencil"></i></a>
+
+                                        <a href="javascript:;" wire:click="delete_sub_category({{$sub_category->id}})" class="text-danger" data-bs-toggle="tooltip"
+                                            data-bs-placement="bottom" title="" data-bs-original-title="Delete"
+                                            aria-label="Delete"><i class="fa-solid fa-trash"></i></a>
+                                    </div>
+                                </td>
+                            </tr>
+                        @endforeach
                     </tbody>
                 </table>
             </div>
