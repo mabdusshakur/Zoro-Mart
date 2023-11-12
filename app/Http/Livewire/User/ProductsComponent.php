07ac94d1226572a5731d52ff81de5a6e56b452fa
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-11-12 22:29:13 +0600

    product order by name ASC , low-to-high || reverse, filter added

--- app/Http/Livewire/User/ProductsComponent.php
+++ app/Http/Livewire/User/ProductsComponent.php
@@ -9,17 +9,30 @@ use Livewire\WithPagination;
 class ProductsComponent extends Component
 { 
     use WithPagination;
-    public $per_page_item;
+    public $per_page_item, $filter_item;
     public $minPrice, $maxPrice;
     
     public function mount()
     {
         $this->minPrice = Product::min('price');
         $this->maxPrice = Product::max('price');
+        $this->per_page_item = 10;
+        $this->filter_item = 'by_name';
     }
     public function render()
     {
-        $products = Product::whereBetween('price', [$this->minPrice, $this->maxPrice])->paginate($this->per_page_item ? $this->per_page_item : 5);
+        if($this->filter_item == 'by_name')
+        {
+            $products = Product::orderBy('name', 'ASC')->whereBetween('price', [$this->minPrice, $this->maxPrice])->paginate($this->per_page_item ? $this->per_page_item : 5);
+        }
+        else if($this->filter_item == 'by_lowest_price')
+        {
+            $products = Product::orderBy('price', 'ASC')->whereBetween('price', [$this->minPrice, $this->maxPrice])->paginate($this->per_page_item ? $this->per_page_item : 5);
+        }
+        else if($this->filter_item == 'by_highest_price')
+        {
+            $products = Product::orderBy('price', 'DESC')->whereBetween('price', [$this->minPrice, $this->maxPrice])->paginate($this->per_page_item ? $this->per_page_item : 5);
+        }
         return view('livewire.user.products-component', ['products' => $products]);
     }
 }
