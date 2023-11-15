715f27d8a83572319d88ee3027fa5295bd5ba4dd
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-11-15 13:04:28 +0600

    bug fixed

--- app/Http/Livewire/User/WishListComponent.php
+++ app/Http/Livewire/User/WishListComponent.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Livewire\User;
 
-use App\Models\Wishlist;
+use App\Models\Cart;
 use Livewire\Component;
+use App\Models\Wishlist;
+use Illuminate\Support\Facades\Auth;
 
 class WishListComponent extends Component
 {
+    public $cart_item_count, $cartItems;
     public function removeProduct($id)
     {
         $wishlist = Wishlist::find($id);
@@ -16,6 +19,8 @@ class WishListComponent extends Component
     public function render()
     {
         $wishlists = Wishlist::where('user_id', auth()->user()->id)->get();
+        $this->cart_item_count = Cart::where('user_id', Auth::user()->id)->count();
+        $this->cartItems = Cart::where('user_id', Auth::user()->id)->get();
         return view('livewire.user.wish-list-component',['wishlists' => $wishlists]);
     }
 }
