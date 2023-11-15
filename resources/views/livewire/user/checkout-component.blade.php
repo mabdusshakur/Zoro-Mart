26abed82a8d63d42dcae9adc7e83d4e590f1c4ae
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-11-15 11:07:39 +0600

    Order total added

--- resources/views/livewire/user/checkout-component.blade.php
+++ resources/views/livewire/user/checkout-component.blade.php
@@ -179,26 +179,9 @@
                                             <th>Cart Subtotal</th>
                                             <td><span class="amount">${{ number_format($sub_total_price , 2)}}</span></td>
                                         </tr>
-                                        <tr class="shipping">
-                                            <th>Shipping</th>
-                                            <td>
-                                                <ul>
-                                                    <li>
-                                                        <input type="radio" name="shipping" />
-                                                        <label>
-                                                            Flat Rate: <span class="amount">$7.00</span>
-                                                        </label>
-                                                    </li>
-                                                    <li>
-                                                        <input type="radio" name="shipping" />
-                                                        <label>Free Shipping:</label>
-                                                    </li>
-                                                </ul>
-                                            </td>
-                                        </tr>
                                         <tr class="order-total">
                                             <th>Order Total</th>
-                                            <td><strong><span class="amount">$215.00</span></strong>
+                                            <td><strong><span class="amount">${{ number_format($sub_total_price , 2)}}</span></strong>
                                             </td>
                                         </tr>
                                     </tfoot>
