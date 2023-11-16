7d047115c4aa224e2354475faeb579088f377174
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-11-16 14:10:08 +0600

    cancel page Cart items added

--- app/Http/Livewire/User/CheckoutCancelComponent.php
+++ app/Http/Livewire/User/CheckoutCancelComponent.php
@@ -2,10 +2,19 @@
 
 namespace App\Http\Livewire\User;
 
+use App\Models\Cart;
 use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
 
 class CheckoutCancelComponent extends Component
 {
+    public $cart_item_count, $cartItems;
+
+    public function mount()
+    {
+        $this->cart_item_count = Cart::where('user_id', Auth::user()->id)->count();
+        $this->cartItems = Cart::where('user_id', Auth::user()->id)->get();
+    }
     public function render()
     {
         return view('livewire.user.checkout-cancel-component');
