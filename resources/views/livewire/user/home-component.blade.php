90d9db5546bcd8fd50437394e3d308c4f0e6e856
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-11-17 12:37:26 +0600

    dealsOfTheDay products added

--- resources/views/livewire/user/home-component.blade.php
+++ resources/views/livewire/user/home-component.blade.php
@@ -166,82 +166,81 @@
     <section class="deal__area pb-40">
         <div class="container">
             <div class="row">
-                <div class="col-xl-6">
+                <div class="col-xl-12">
                     <div class="section__head d-md-flex justify-content-between mb-40">
                         <div class="section__title">
                             <h3>Deals<span>Of The Day</span></h3>
                         </div>
                     </div>
                     <div class="product__deal owl-carousel">
-                        <div class="product__deal-item">
-                            <div class="product__item product__item-2 product__sale mb-30">
-                                <div class="row">
-                                    <div class="col-xl-6  col-lg-6 col-md-6 col-sm-6">
-                                        <div class="product__thumb product__thumb-big p-relative">
-                                            <a href="product-details.html" class="w-img">
-                                                <img src="{{ asset('user/assets/img/shop/product/product-15.jpg') }}"
-                                                    alt="product" />
-                                                <img class="second-img"
-                                                    src="{{ asset('user/assets/img/shop/product/product-24.jpg') }}"
-                                                    alt="product" />
-                                            </a>
-                                            <div class="product__offer">
-                                                <span class="discount">-34%</span>
-                                            </div>
+                      @foreach ($products as $product)
+                      <div class="product__deal-item">
+                        <div class="product__item product__item-2 product__sale mb-30">
+                            <div class="row">
+                                <div class="col-xl-6  col-lg-6 col-md-6 col-sm-6">
+                                    <div class="product__thumb product__thumb-big p-relative">
+                                        <a href="product-details.html" class="w-img">
+                                            <img src="{{ asset('user/assets/img/shop/product/product-15.jpg') }}"
+                                                alt="product" />
+                                            <img class="second-img"
+                                                src="{{ asset('user/assets/img/shop/product/product-24.jpg') }}"
+                                                alt="product" />
+                                        </a>
+                                        <div class="product__offer">
+                                            <span class="discount">-34%</span>
                                         </div>
                                     </div>
-                                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6">
-                                        <div class="product__content product__content-2">
-                                            <h6 class="product-name product__deal-name">
-                                                <a class="product-item-link" href="product-details.html">
-                                                    Original Mobile Android Dual SIM Smart Phone G3 </a>
-                                            </h6>
-                                            <div class="rating rating-2">
-                                                <ul>
-                                                    <li>
-                                                        <a href="#"><i class="far fa-star"></i></a>
-                                                    </li>
-                                                    <li>
-                                                        <a href="#"><i class="far fa-star"></i></a>
-                                                    </li>
-                                                    <li>
-                                                        <a href="#"><i class="far fa-star"></i></a>
-                                                    </li>
-                                                    <li>
-                                                        <a href="#"><i class="far fa-star"></i></a>
-                                                    </li>
-                                                    <li>
-                                                        <a href="#"><i class="far fa-star"></i></a>
-                                                    </li>
-                                                </ul>
-                                            </div>
-                                            <span class="new mb-5">$120.00</span>
-                                            <span class="price-old mb-5"> <del>$125.00</del> </span>
-                                            <p class="mt-10">Typi non habent claritatem insitam, est usus
-                                                legentis in iis qui facit...</p>
-                                            <div class="product__countdown">
-                                                <h4>Hurry Up! Offer ends in:</h4>
-                                                <div class="countdown-wrapper">
-                                                    <div data-countdown data-date="Dec 02 2022 20:20:22">
-                                                        <ul>
-                                                            <li>
-                                                                <span data-days>0</span>
-                                                                <p>Days</p>
-                                                            </li>
-                                                            <li>
-                                                                <span data-hours>0</span>
-                                                                <p>Hours</p>
-                                                            </li>
-                                                            <li>
-                                                                <span data-minutes>0</span>
-                                                                <p>mins</p>
-                                                            </li>
-                                                            <li>
-                                                                <span data-seconds>0</span>
-                                                                <p>secs</p>
-                                                            </li>
-                                                        </ul>
-                                                    </div>
+                                </div>
+                                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6">
+                                    <div class="product__content product__content-2">
+                                        <h6 class="product-name product__deal-name">
+                                            <a class="product-item-link" href="product-details.html">
+                                                {{$product->name}} </a>
+                                        </h6>
+                                        <div class="rating rating-2">
+                                            <ul>
+                                                <li>
+                                                    <a href="#"><i class="far fa-star"></i></a>
+                                                </li>
+                                                <li>
+                                                    <a href="#"><i class="far fa-star"></i></a>
+                                                </li>
+                                                <li>
+                                                    <a href="#"><i class="far fa-star"></i></a>
+                                                </li>
+                                                <li>
+                                                    <a href="#"><i class="far fa-star"></i></a>
+                                                </li>
+                                                <li>
+                                                    <a href="#"><i class="far fa-star"></i></a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                        <span class="new mb-5">${{$product->discount_price}}</span>
+                                        <span class="price-old mb-5"> <del>${{$product->price}}</del> </span>
+                                        <p class="mt-10">{{$product->description}}</p>
+                                        <div class="product__countdown">
+                                            <h4>Hurry Up! Offer ends in:</h4>
+                                            <div class="countdown-wrapper">
+                                                <div data-countdown data-date="Dec 02 2022 20:20:22">
+                                                    <ul>
+                                                        <li>
+                                                            <span data-days>0</span>
+                                                            <p>Days</p>
+                                                        </li>
+                                                        <li>
+                                                            <span data-hours>0</span>
+                                                            <p>Hours</p>
+                                                        </li>
+                                                        <li>
+                                                            <span data-minutes>0</span>
+                                                            <p>mins</p>
+                                                        </li>
+                                                        <li>
+                                                            <span data-seconds>0</span>
+                                                            <p>secs</p>
+                                                        </li>
+                                                    </ul>
                                                 </div>
                                             </div>
                                         </div>
@@ -249,82 +248,8 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="product__deal-item">
-                            <div class="product__item product__item-2 product__sale mb-30">
-                                <div class="row">
-                                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6">
-                                        <div class="product__thumb product__thumb-big p-relative">
-                                            <a href="product-details.html" class="w-img">
-                                                <img src="{{ asset('user/assets/img/shop/product/product-15.jpg') }}"
-                                                    alt="product" />
-                                                <img class="second-img"
-                                                    src="{{ asset('user/assets/img/shop/product/product-24.jpg') }}"
-                                                    alt="product" />
-                                            </a>
-                                            <div class="product__offer">
-                                                <span class="discount">-34%</span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6">
-                                        <div class="product__content product__content-2">
-                                            <h6 class="product-name product__deal-name">
-                                                <a class="product-item-link" href="product-details.html">
-                                                    Original Mobile Android Dual SIM Smart Phone G3 </a>
-                                            </h6>
-                                            <div class="rating rating-2">
-                                                <ul>
-                                                    <li>
-                                                        <a href="#"><i class="far fa-star"></i></a>
-                                                    </li>
-                                                    <li>
-                                                        <a href="#"><i class="far fa-star"></i></a>
-                                                    </li>
-                                                    <li>
-                                                        <a href="#"><i class="far fa-star"></i></a>
-                                                    </li>
-                                                    <li>
-                                                        <a href="#"><i class="far fa-star"></i></a>
-                                                    </li>
-                                                    <li>
-                                                        <a href="#"><i class="far fa-star"></i></a>
-                                                    </li>
-                                                </ul>
-                                            </div>
-                                            <span class="new mb-5">$120.00</span>
-                                            <span class="price-old mb-5"> <del>$125.00</del> </span>
-                                            <p class="mt-10">Typi non habent claritatem insitam, est usus
-                                                legentis in iis qui facit...</p>
-                                            <div class="product__countdown">
-                                                <h4>Hurry Up! Offer ends in:</h4>
-                                                <div class="countdown-wrapper">
-                                                    <div data-countdown data-date="Dec 02 2022 20:20:22">
-                                                        <ul>
-                                                            <li>
-                                                                <span data-days>0</span>
-                                                                <p>Days</p>
-                                                            </li>
-                                                            <li>
-                                                                <span data-hours>0</span>
-                                                                <p>Hours</p>
-                                                            </li>
-                                                            <li>
-                                                                <span data-minutes>0</span>
-                                                                <p>mins</p>
-                                                            </li>
-                                                            <li>
-                                                                <span data-seconds>0</span>
-                                                                <p>secs</p>
-                                                            </li>
-                                                        </ul>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                    </div>
+                      @endforeach
                     </div>
                 </div>
             </div>
