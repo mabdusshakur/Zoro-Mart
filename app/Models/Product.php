47d93a0b1de7b9f77e7ce93cc01229c74e5be995
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-11-17 11:38:43 +0600

    product model fillable added

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,5 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
-    protected $fillable = ['name', 'slug', 'description', 'price', 'quantity', 'image', 'category_id', 'sub_category_id', 'product_uid'];
+    protected $fillable = ['name', 'slug', 'description', 'price', 'quantity', 'image', 'category_id', 'sub_category_id', 'product_uid', 'sold', 'discount_price', 'on_sale'];
 }
