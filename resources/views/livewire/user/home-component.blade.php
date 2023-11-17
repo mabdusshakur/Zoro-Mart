30dd2787d3d81747d31379f27bcd71ff61c0203f
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-11-17 12:38:59 +0600

    day Deals customize

--- resources/views/livewire/user/home-component.blade.php
+++ resources/views/livewire/user/home-component.blade.php
@@ -166,90 +166,92 @@
     <section class="deal__area pb-40">
         <div class="container">
             <div class="row">
-                <div class="col-xl-12">
+                <div class="col-xl-6">
                     <div class="section__head d-md-flex justify-content-between mb-40">
                         <div class="section__title">
                             <h3>Deals<span>Of The Day</span></h3>
                         </div>
                     </div>
                     <div class="product__deal owl-carousel">
-                      @foreach ($products as $product)
-                      <div class="product__deal-item">
-                        <div class="product__item product__item-2 product__sale mb-30">
-                            <div class="row">
-                                <div class="col-xl-6  col-lg-6 col-md-6 col-sm-6">
-                                    <div class="product__thumb product__thumb-big p-relative">
-                                        <a href="product-details.html" class="w-img">
-                                            <img src="{{ asset('user/assets/img/shop/product/product-15.jpg') }}"
-                                                alt="product" />
-                                            <img class="second-img"
-                                                src="{{ asset('user/assets/img/shop/product/product-24.jpg') }}"
-                                                alt="product" />
-                                        </a>
-                                        <div class="product__offer">
-                                            <span class="discount">-34%</span>
+                        @foreach ($products as $product)
+                            @php
+                                $images = $images = \App\Models\product_image::where('product_uid', $product->product_uid)->get();
+                            @endphp
+                            <div class="product__deal-item">
+                                <div class="product__item product__item-2 product__sale mb-30">
+                                    <div class="row">
+                                        <div class="col-xl-6  col-lg-6 col-md-6 col-sm-6">
+                                            <div class="product__thumb product__thumb-big p-relative">
+                                                <a href="product-details.html" class="w-img">
+                                                    <img src="{{ Storage::url($images[0]->image) }}" alt="product" />
+                                                <img class="second-img"
+                                                    src="{{ count($images) > 1 ? Storage::url($images[1]->image) : Storage::url($images[0]->image) }}"
+                                                    alt="product" />
+                                                </a>
+                                                <div class="product__offer">
+                                                    <span class="discount">-34%</span>
+                                                </div>
+                                            </div>
                                         </div>
-                                    </div>
-                                </div>
-                                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6">
-                                    <div class="product__content product__content-2">
-                                        <h6 class="product-name product__deal-name">
-                                            <a class="product-item-link" href="product-details.html">
-                                                {{$product->name}} </a>
-                                        </h6>
-                                        <div class="rating rating-2">
-                                            <ul>
-                                                <li>
-                                                    <a href="#"><i class="far fa-star"></i></a>
-                                                </li>
-                                                <li>
-                                                    <a href="#"><i class="far fa-star"></i></a>
-                                                </li>
-                                                <li>
-                                                    <a href="#"><i class="far fa-star"></i></a>
-                                                </li>
-                                                <li>
-                                                    <a href="#"><i class="far fa-star"></i></a>
-                                                </li>
-                                                <li>
-                                                    <a href="#"><i class="far fa-star"></i></a>
-                                                </li>
-                                            </ul>
-                                        </div>
-                                        <span class="new mb-5">${{$product->discount_price}}</span>
-                                        <span class="price-old mb-5"> <del>${{$product->price}}</del> </span>
-                                        <p class="mt-10">{{$product->description}}</p>
-                                        <div class="product__countdown">
-                                            <h4>Hurry Up! Offer ends in:</h4>
-                                            <div class="countdown-wrapper">
-                                                <div data-countdown data-date="Dec 02 2022 20:20:22">
+                                        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6">
+                                            <div class="product__content product__content-2">
+                                                <h6 class="product-name product__deal-name">
+                                                    <a class="product-item-link" href="product-details.html">
+                                                        {{ $product->name }} </a>
+                                                </h6>
+                                                <div class="rating rating-2">
                                                     <ul>
                                                         <li>
-                                                            <span data-days>0</span>
-                                                            <p>Days</p>
+                                                            <a href="#"><i class="far fa-star"></i></a>
                                                         </li>
                                                         <li>
-                                                            <span data-hours>0</span>
-                                                            <p>Hours</p>
+                                                            <a href="#"><i class="far fa-star"></i></a>
                                                         </li>
                                                         <li>
-                                                            <span data-minutes>0</span>
-                                                            <p>mins</p>
+                                                            <a href="#"><i class="far fa-star"></i></a>
                                                         </li>
                                                         <li>
-                                                            <span data-seconds>0</span>
-                                                            <p>secs</p>
+                                                            <a href="#"><i class="far fa-star"></i></a>
+                                                        </li>
+                                                        <li>
+                                                            <a href="#"><i class="far fa-star"></i></a>
                                                         </li>
                                                     </ul>
+                                                </div>
+                                                <span class="new mb-5">${{ $product->discount_price }}</span>
+                                                <span class="price-old mb-5"> <del>${{ $product->price }}</del> </span>
+                                                <p class="mt-10">{{ $product->description }}</p>
+                                                <div class="product__countdown">
+                                                    <h4>Hurry Up! Offer ends in:</h4>
+                                                    <div class="countdown-wrapper">
+                                                        <div data-countdown data-date="Dec 02 2022 20:20:22">
+                                                            <ul>
+                                                                <li>
+                                                                    <span data-days>0</span>
+                                                                    <p>Days</p>
+                                                                </li>
+                                                                <li>
+                                                                    <span data-hours>0</span>
+                                                                    <p>Hours</p>
+                                                                </li>
+                                                                <li>
+                                                                    <span data-minutes>0</span>
+                                                                    <p>mins</p>
+                                                                </li>
+                                                                <li>
+                                                                    <span data-seconds>0</span>
+                                                                    <p>secs</p>
+                                                                </li>
+                                                            </ul>
+                                                        </div>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    </div>
-                      @endforeach
+                        @endforeach
                     </div>
                 </div>
             </div>
@@ -274,47 +276,48 @@
                     <div class="sale__slider owl-carousel">
                         @foreach ($products as $product)
                             @if ($product->on_sale == true)
-                            @php
-                                $images = $images = \App\Models\product_image::where('product_uid', $product->product_uid)->get();
-                            @endphp
-                            <div class="product__item-wrapper">
-                                <div class="product__item product__item-2 white-bg d-flex mb-20">
-                                    <div class="product__thumb product__thumb-sale p-relative">
-                                        <a href="product-details.html" class="w-img">
-                                            <img src="{{ Storage::url($images[0]->image) }}" alt="product" />
-                                            <img class="second-img"
-                                                src="{{ count($images) > 1 ? Storage::url($images[1]->image) : Storage::url($images[0]->image) }}"
-                                                alt="product" />
-                                        </a>
-                                    </div>
-                                    <div class="product__content">
-                                        <h6 class="product-name">
-                                            <a class="product-item-link" href="product-details.html">{{$product->name}}</a>
-                                        </h6>
-                                        <div class="rating">
-                                            <ul>
-                                                <li>
-                                                    <a href="#"><i class="far fa-star"></i></a>
-                                                </li>
-                                                <li>
-                                                    <a href="#"><i class="far fa-star"></i></a>
-                                                </li>
-                                                <li>
-                                                    <a href="#"><i class="far fa-star"></i></a>
-                                                </li>
-                                                <li>
-                                                    <a href="#"><i class="far fa-star"></i></a>
-                                                </li>
-                                                <li>
-                                                    <a href="#"><i class="far fa-star"></i></a>
-                                                </li>
-                                            </ul>
+                                @php
+                                    $images = $images = \App\Models\product_image::where('product_uid', $product->product_uid)->get();
+                                @endphp
+                                <div class="product__item-wrapper">
+                                    <div class="product__item product__item-2 white-bg d-flex mb-20">
+                                        <div class="product__thumb product__thumb-sale p-relative">
+                                            <a href="product-details.html" class="w-img">
+                                                <img src="{{ Storage::url($images[0]->image) }}" alt="product" />
+                                                <img class="second-img"
+                                                    src="{{ count($images) > 1 ? Storage::url($images[1]->image) : Storage::url($images[0]->image) }}"
+                                                    alt="product" />
+                                            </a>
                                         </div>
-                                        <span class="new new-2">${{$product->discount_price}}</span>
-                                        <span class="price-old"> <del>${{$product->price}}</del> </span>
+                                        <div class="product__content">
+                                            <h6 class="product-name">
+                                                <a class="product-item-link"
+                                                    href="product-details.html">{{ $product->name }}</a>
+                                            </h6>
+                                            <div class="rating">
+                                                <ul>
+                                                    <li>
+                                                        <a href="#"><i class="far fa-star"></i></a>
+                                                    </li>
+                                                    <li>
+                                                        <a href="#"><i class="far fa-star"></i></a>
+                                                    </li>
+                                                    <li>
+                                                        <a href="#"><i class="far fa-star"></i></a>
+                                                    </li>
+                                                    <li>
+                                                        <a href="#"><i class="far fa-star"></i></a>
+                                                    </li>
+                                                    <li>
+                                                        <a href="#"><i class="far fa-star"></i></a>
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                            <span class="new new-2">${{ $product->discount_price }}</span>
+                                            <span class="price-old"> <del>${{ $product->price }}</del> </span>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
                             @endif
                         @endforeach
                     </div>
