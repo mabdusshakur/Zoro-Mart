5f514eaea0d368a9e1c404721b90a396cdeed74a
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-11-18 13:23:51 +0600

    product details page Product name des price added

--- app/Http/Livewire/User/ProductDetailsComponent.php
+++ app/Http/Livewire/User/ProductDetailsComponent.php
@@ -3,17 +3,27 @@
 namespace App\Http\Livewire\User;
 
 use App\Models\Cart;
+use App\Models\Product;
 use Livewire\Component;
 use Illuminate\Support\Facades\Auth;
 
 class ProductDetailsComponent extends Component
 {
     public $cart_item_count, $cartItems;
-    public $product_name, $product_description, $product_price, $product_quantity, $product_uid , $product_id;
-    public function render()
+    public $product_name, $product_description, $product_price, $product_quantity, $product_uid, $product_id;
+    
+    public $products;
+
+    public function mount($id, $slug = null, $category_id = null, $sub_category_id = null)
     {
+        $this->products = Product::where('id', $id)->get();
+        
         $this->cart_item_count = Cart::where('user_id', Auth::user()->id)->count();
         $this->cartItems = Cart::where('user_id', Auth::user()->id)->get();
+    }
+
+    public function render()
+    {
         return view('livewire.user.product-details-component');
     }
 }
--- resources/views/livewire/user/product-details-component.blade.php
+++ resources/views/livewire/user/product-details-component.blade.php
@@ -110,105 +110,99 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-xxl-7 col-xl-7 col-lg-7">
-                    <div class="product__details-wrapper">
-                        <div class="product__details">
-                            <h3 class="product__details-title">
-                                <a href="product-details.html">Smart Mobile Phone 7/7plus/8/8plus/X/Xr W 32 to
-                                    128GB</a>
-                            </h3>
-                            <div class="product__review d-sm-flex">
-                                <div class="rating rating__shop mb-15 mr-35">
-                                    <ul>
-                                        <li><a href="#"><i class="fal fa-star"></i></a></li>
-                                        <li><a href="#"><i class="fal fa-star"></i></a></li>
-                                        <li><a href="#"><i class="fal fa-star"></i></a></li>
-                                        <li><a href="#"><i class="fal fa-star"></i></a></li>
-                                        <li><a href="#"><i class="fal fa-star"></i></a></li>
-                                    </ul>
-                                </div>
-                                <div class="product__add-review mb-15">
-                                    <span><a href="#">1 Review</a></span>
-                                    <span><a href="#">Add Review</a></span>
-                                </div>
-                            </div>
-                            <div class="product__price">
-                                <span class="new">$150.00</span>
-                                <span class="old">$320.00</span>
-                            </div>
-                            <div class="product__stock">
-                                <span>Availability :</span>
-                                <span>In Stock</span>
-                            </div>
-                            <div class="product__stock sku mb-30">
-                                <span>SKU:</span>
-                                <span>Samsung C49J89: £875, Debenhams Plus</span>
-                            </div>
-                            <div class="product__details-des mb-30">
-                                <p>Typi non habent claritatem insitam, est usus legentis in iis qui facit eorum
-                                    claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt
-                                    saepius. Claritas est etiam processus. Lorem Ipsum has been the industry's standard
-                                    dummy text ever since the 1500s, when anunknown printer took a galley of type and
-                                    scrambled it to make a type specimen book.</p>
-                            </div>
-                            <div class="product__details-stock">
-                                <h3><span>Hurry Up!</span> Only 98 products left in stock.</h3>
-                                <div class="progress">
-                                    <div class="progress-bar progress-bar-striped progress-bar-animated"
-                                        role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"
-                                        data-width="100%"></div>
-                                </div>
-                            </div>
-                            <div class="product__details-color d-flex align-items-center mb-15">
-                                <span>Colors</span>
+                
+              @foreach ($products as $product)
+              <div class="col-xxl-7 col-xl-7 col-lg-7">
+                <div class="product__details-wrapper">
+                    <div class="product__details">
+                        <h3 class="product__details-title">
+                            <a href="product-details.html">{{$product->name}}</a>
+                        </h3>
+                        <div class="product__review d-sm-flex">
+                            <div class="rating rating__shop mb-15 mr-35">
                                 <ul>
-                                    <li>
-                                        <a href="#"></a>
-                                    </li>
-                                    <li>
-                                        <a href="#" class="blue"></a>
-                                    </li>
-                                    <li>
-                                        <a href="#" class="red"></a>
-                                    </li>
+                                    <li><a href="#"><i class="fal fa-star"></i></a></li>
+                                    <li><a href="#"><i class="fal fa-star"></i></a></li>
+                                    <li><a href="#"><i class="fal fa-star"></i></a></li>
+                                    <li><a href="#"><i class="fal fa-star"></i></a></li>
+                                    <li><a href="#"><i class="fal fa-star"></i></a></li>
                                 </ul>
                             </div>
-                            <div class="product__details-size d-flex align-items-center mb-25">
-                                <span>Size</span>
-                                <ul>
-                                    <li>
-                                        <a href="#">55</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">65</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">70</a>
-                                    </li>
-                                </ul>
+                            <div class="product__add-review mb-15">
+                                <span><a href="#">1 Review</a></span>
+                                <span><a href="#">Add Review</a></span>
                             </div>
-                            <div class="product__details-quantity mb-20">
-                                <form action="#">
-                                    <div class="pro-quan-area d-lg-flex align-items-center">
-                                        <div class="product-quantity mr-20 mb-25">
-                                            <div class="cart-plus-minus p-relative"><input type="text"
-                                                    value="1" /></div>
-                                        </div>
-                                        <div class="pro-cart-btn mb-25">
-                                            <button class="t-y-btn" type="submit">Add to cart</button>
-                                        </div>
+                        </div>
+                        <div class="product__price">
+                            <span class="new">${{$product->price}}</span>
+                            <span class="old">${{$product->discount_price}}</span>
+                        </div>
+                        <div class="product__stock">
+                            <span>Availability :</span>
+                            <span>In Stock</span>
+                        </div>
+                        <div class="product__details-des mb-30">
+                            <p>{{$product->description}}</p>
+                        </div>
+                        <div class="product__details-stock">
+                            <h3><span>Hurry Up!</span> Only 98 products left in stock.</h3>
+                            <div class="progress">
+                                <div class="progress-bar progress-bar-striped progress-bar-animated"
+                                    role="progressbar" aria-valuemin="0" aria-valuemax="100"
+                                    data-width="70%"></div>
+                            </div>
+                        </div>
+                        <div class="product__details-color d-flex align-items-center mb-15">
+                            <span>Colors</span>
+                            <ul>
+                                <li>
+                                    <a href="#"></a>
+                                </li>
+                                <li>
+                                    <a href="#" class="blue"></a>
+                                </li>
+                                <li>
+                                    <a href="#" class="red"></a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="product__details-size d-flex align-items-center mb-25">
+                            <span>Size</span>
+                            <ul>
+                                <li>
+                                    <a href="#">55</a>
+                                </li>
+                                <li>
+                                    <a href="#">65</a>
+                                </li>
+                                <li>
+                                    <a href="#">70</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="product__details-quantity mb-20">
+                            <form action="#">
+                                <div class="pro-quan-area d-lg-flex align-items-center">
+                                    <div class="product-quantity mr-20 mb-25">
+                                        <div class="cart-plus-minus p-relative"><input type="text"
+                                                value="1" /></div>
                                     </div>
-                                </form>
-                            </div>
-                            <div class="product__details-action">
-                                <ul>
-                                    <li><a href="#" title="Add to Wishlist"><i class="fal fa-heart"></i></a></li>
-                                    <li><a href="#" title="Share"><i class="fal fa-share-alt"></i></a></li>
-                                </ul>
-                            </div>
+                                    <div class="pro-cart-btn mb-25">
+                                        <button class="t-y-btn" type="submit">Add to cart</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                        <div class="product__details-action">
+                            <ul>
+                                <li><a href="#" title="Add to Wishlist"><i class="fal fa-heart"></i></a></li>
+                                <li><a href="#" title="Share"><i class="fal fa-share-alt"></i></a></li>
+                            </ul>
                         </div>
                     </div>
                 </div>
+                </div>
+              @endforeach
             </div>
             <div class="row">
                 <div class="col-xxl-12">
