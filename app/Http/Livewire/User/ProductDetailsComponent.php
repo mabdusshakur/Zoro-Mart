82f689a2d86731cc1ca9aa8054e644baac615a07
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-11-18 13:02:09 +0600

    product details page activated

--- app/Http/Livewire/User/ProductDetailsComponent.php
+++ app/Http/Livewire/User/ProductDetailsComponent.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Livewire\User;
 
+use App\Models\Cart;
 use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
 
 class ProductDetailsComponent extends Component
 {
+    public $cart_item_count, $cartItems;
+    public $product_name, $product_description, $product_price, $product_quantity, $product_uid , $product_id;
     public function render()
     {
+        $this->cart_item_count = Cart::where('user_id', Auth::user()->id)->count();
+        $this->cartItems = Cart::where('user_id', Auth::user()->id)->get();
         return view('livewire.user.product-details-component');
     }
 }
