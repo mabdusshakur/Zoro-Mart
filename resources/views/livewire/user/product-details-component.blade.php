2f55a48baed619971f097ef530529504f6c17d71
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-11-18 13:02:38 +0600

    Product details Brand removed

--- resources/views/livewire/user/product-details-component.blade.php
+++ resources/views/livewire/user/product-details-component.blade.php
@@ -814,31 +814,6 @@
     </section>
     <!-- product area end -->
 
-    <!-- brand area start -->
-    <section class="brand__area">
-        <div class="container custom-container">
-            <div class="row align-items-center">
-                <div class="col-xl-12">
-                    <div class="brand__slider owl-carousel">
-                        <div class="brand__item">
-                            <img src="{{ 'user/assets/img/brand/brand-1.jpg' }}" alt="">
-                        </div>
-                        <div class="brand__item">
-                            <img src="{{ 'user/assets/img/brand/brand-2.jpg' }}" alt="">
-                        </div>
-                        <div class="brand__item">
-                            <img src="{{ 'user/assets/img/brand/brand-3.jpg' }}" alt="">
-                        </div>
-                        <div class="brand__item">
-                            <img src="{{ 'user/assets/img/brand/brand-4.jpg' }}" alt="">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <!-- brand area end -->
-
     <!-- shop modal start -->
     @include('partials.user._shop_modal')
     <!-- shop modal end -->
