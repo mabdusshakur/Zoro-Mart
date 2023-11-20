054d1b8692b5ab8f728e312d5a8ed09f405de2a5
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-11-20 12:49:25 +0600

    star system added

--- resources/views/livewire/user/product-details-component.blade.php
+++ resources/views/livewire/user/product-details-component.blade.php
@@ -177,20 +177,28 @@
                                                         <div class="review-ratings-single d-flex align-items-center">
                                                             <span>Quality</span>
                                                             <ul>
-                                                                @for ($i = 0; $i < $review->rating; $i++)
-                                                                    <li><a><i class="fal fa-star"></i></a></li>
-                                                                    <li><a><i class="fal fa-star" style="color: #e6c002;"></i></a></li>
+                                                                @for ($i = 0; $i < 5; $i++)
+                                                                    @if ($i < $review->rating)
+                                                                        <li><a><i class="fa-solid fa-star"></i></a>
+                                                                        </li>
+                                                                    @else
+                                                                        <li>
+                                                                            <a>
+                                                                                <i class="fa-regular fa-star"></i>
+                                                                            </a>
+                                                                        </li>
+                                                                    @endif
                                                                 @endfor
                                                             </ul>
                                                         </div>
                                                     </div>
                                                     <div class="review-text">
-                                                        <p>{{$review->review}}</p>
+                                                        <p>{{ $review->review }}</p>
                                                     </div>
                                                     <div class="review-meta">
                                                         <div class="review-author">
                                                             <span>Review By : </span>
-                                                            <span>{{$review->user->first_name}}</span>
+                                                            <span>{{ $review->user->first_name }}</span>
                                                         </div>
                                                         <div class="review-date">
                                                             <span>Posted on</span>
