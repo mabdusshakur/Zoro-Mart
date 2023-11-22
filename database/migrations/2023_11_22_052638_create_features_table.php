827220cf5ee7f7b9a8117ca283d61ef4ff487c9d
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-11-22 11:32:33 +0600

    features Table added

---
+++ database/migrations/2023_11_22_052638_create_features_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('features', function (Blueprint $table) {
+            $table->id();
+            $table->string("title")->max(26);
+            $table->string("description")->max(62);
+            $table->string("icon")->max(26);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('features');
+    }
+};
