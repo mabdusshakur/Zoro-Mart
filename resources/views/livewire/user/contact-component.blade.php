fbbc805063f6ac42a02d8a04e5dafa5b5684600c
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-11-27 10:39:19 +0600

    Contact form working added

--- resources/views/livewire/user/contact-component.blade.php
+++ resources/views/livewire/user/contact-component.blade.php
@@ -4,26 +4,26 @@
     @include('partials.user._alerts')
     <div class="container">
         <div class="d-flex justify-content-center mt-4">
-            <form style="width: 26rem;">
+            <form wire:submit.prevent="contact" style="width: 26rem;">
                 <!-- Name input -->
                 <div data-mdb-input-init class="form-outline mb-4">
-                    <input type="text" id="form4Example1" class="form-control" />
+                    <input type="text" id="form4Example1" class="form-control" wire:model="name" />
                     <label class="form-label" for="form4Example1">Name</label>
                 </div>
     
                 <!-- Email input -->
                 <div data-mdb-input-init class="form-outline mb-4">
-                    <input type="email" id="form4Example2" class="form-control" />
+                    <input type="email" id="form4Example2" class="form-control" wire:model="email" />
                     <label class="form-label" for="form4Example2">Email address</label>
                 </div>
     
                 <!-- Message input -->
                 <div data-mdb-input-init class="form-outline mb-4">
-                    <textarea class="form-control" id="form4Example3" rows="4"></textarea>
+                    <textarea class="form-control" id="form4Example3" rows="4" wire:model="message" ></textarea>
                     <label class="form-label" for="form4Example3">Message</label>
                 </div>
                 <!-- Submit button -->
-                <button data-mdb-ripple-init type="button" class="btn btn-primary btn-block mb-4">Send</button>
+                <button data-mdb-ripple-init type="submit" class="btn btn-primary btn-block mb-4">Send</button>
             </form>
         </div>
     </div>
