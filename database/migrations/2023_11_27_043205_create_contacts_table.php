5603a12f8f114e2bc696ba3b10b6c6432e00cb46
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-11-27 10:53:56 +0600

    status field added on contact table

--- database/migrations/2023_11_27_043205_create_contacts_table.php
+++ database/migrations/2023_11_27_043205_create_contacts_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string("name");
             $table->string("email");
             $table->longText("message");
+            $table->boolean("status")->default(0);
             $table->timestamps();
         });
     }
