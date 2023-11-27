b2cd1457cc1eeb7525db205c05e75c54604619c1
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-11-27 10:36:01 +0600

    Contact backend added

--- app/Http/Livewire/User/ContactComponent.php
+++ app/Http/Livewire/User/ContactComponent.php
@@ -2,11 +2,28 @@
 
 namespace App\Http\Livewire\User;
 
+use App\Models\Contact;
 use Livewire\Component;
 
 class ContactComponent extends Component
 {
     public $hotline;
+    public $name, $email, $message;
+
+    public function contact()
+    {
+        $this->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+            'message' => 'required|min:10'
+        ]);
+        $contact = new Contact();
+        $contact->name = $this->name;
+        $contact->email = $this->email;
+        $contact->message = $this->message;
+        $contact->save();
+        session()->flash('message', 'Your message has been sent successfully!');
+    }
     public function render()
     {
         return view('livewire.user.contact-component');
