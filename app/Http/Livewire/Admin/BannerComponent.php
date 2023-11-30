2b81221468d015686d7cb7cb0ff73225cc2ee67d
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-11-30 12:59:04 +0600

    Add Banner backend added

--- app/Http/Livewire/Admin/BannerComponent.php
+++ app/Http/Livewire/Admin/BannerComponent.php
@@ -2,12 +2,41 @@
 
 namespace App\Http\Livewire\Admin;
 
+use App\Models\Banner;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class BannerComponent extends Component
 {
+    public $banner_title, $banner, $banner_status;
+
+    use WithFileUploads;
+
+    public function add_banner()
+    {
+        $this->validate([
+            'banner_title' => 'required',
+            'banner' => 'required|image|mimes:jpeg,png,jpg,gif,svg',
+            'banner_status' => 'required'
+        ]);
+        $banner = new Banner();
+        $banner->banner_title = $this->banner_title;
+        $imageName = time() . '.' . $this->banner->getClientOriginalExtension();
+        $img_location = $this->banner->storeAs('banner/images', $imageName, 'public');
+        $banner->banner_image = $img_location;
+        $banner->banner_status = $this->banner_status;
+        $banner->save();
+        session()->flash('success', 'Banner has been created successfully!');
+    }
+    public function delete_banner($id)
+    {
+        $banner = Banner::find($id);
+        $banner->delete();
+        session()->flash('success', 'Banner has been deleted successfully!');
+    }
     public function render()
     {
-        return view('livewire.admin.banner-component')->layout('layouts.admin');
+        $banners = Banner::all();
+        return view('livewire.admin.banner-component', ['banners' => $banners])->layout('layouts.admin');
     }
 }
--- resources/views/livewire/admin/banner-component.blade.php
+++ resources/views/livewire/admin/banner-component.blade.php
@@ -1,6 +1,6 @@
 <div>
     <!--start breadcrumb-->
-    @section('current-page-name', 'Add Product')
+    @section('current-page-name', 'Banners')
     @include('partials.admin._bread_crumb')
     <!--end breadcrumb-->
 
@@ -24,6 +24,7 @@
                 <div class="col-md-4 mb-4">
                     <label for="banner_status" class="form-label">Select Banner</label>
                     <select class="form-select" id="banner_status" wire:model="banner_status">
+                        <option value="">Select Status</option>
                         <option value="1">Enabled</option>
                         <option value="0">Disabled</option>
                     </select>
@@ -35,4 +36,41 @@
             </div>
         </form>
     </div>
+
+    <div class="card">
+        <div class="card-body">
+            <div class="d-flex align-items-center">
+                <h5 class="mb-0">All Banners</h5>
+                <form class="ms-auto position-relative">
+                    <div class="position-absolute top-50 translate-middle-y search-icon px-3"><i class="fa-solid fa-magnifying-glass"></i></div>
+                    <input class="form-control ps-5" type="text" placeholder="Product Nameor Or Id" wire:model="search">
+                </form>
+            </div>
+            <div class="table-responsive mt-3">
+                <table class="table align-middle mb-0">
+                    <thead class="table-light">
+                        <tr>
+                            <th>Title</th>
+                            <th>Photo</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($banners as $banner)
+                        <tr>
+                            <td>{{$banner->banner_title}}</td>
+                            <td><img src="{{Storage::url($banner->banner_image)}}" class="product-img-2" alt="banner img"></td>
+                            <td>
+                                <div class="table-actions d-flex align-items-center gap-3 fs-6">
+                                    <a href="javascript:;" wire:click="delete_banner({{$banner->id}})" class="text-danger" data-bs-toggle="tooltip" data-bs-placement="bottom" title="" data-bs-original-title="Delete" aria-label="Delete"><i class="fa-solid fa-trash"></i></a>
+                                </div>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
 </div>
