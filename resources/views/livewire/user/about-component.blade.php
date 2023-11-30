5d30eb46c7659686327c67820509dfeda8d8a41c
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-11-30 13:13:29 +0600

    custimization

--- resources/views/livewire/user/about-component.blade.php
+++ resources/views/livewire/user/about-component.blade.php
@@ -9,7 +9,7 @@
                 <h1>About Us</h1>
             </div>
             <div class="col-md-12">
-                <p>
+                <p class="text-center">
                     {{$about}}
                 </p>
             </div>
