17bce7712c9ccec982bcdd48e61a966e9c41d446
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-11-30 13:08:17 +0600

    cart and searchbar added for Cotact form

--- app/Http/Livewire/User/ContactComponent.php
+++ app/Http/Livewire/User/ContactComponent.php
@@ -2,14 +2,19 @@
 
 namespace App\Http\Livewire\User;
 
+use App\Models\Cart;
 use App\Models\Contact;
+use App\Models\Product;
 use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
 
 class ContactComponent extends Component
 {
     public $hotline;
     public $name, $email, $message;
 
+    public $cart_item_count, $cartItems;
+    public $main_search;
     public function contact()
     {
         $this->validate([
@@ -27,6 +32,12 @@ class ContactComponent extends Component
     }
     public function render()
     {
-        return view('livewire.user.contact-component');
+        $live_search_products = [];
+        if ($this->main_search != null) {
+            $live_search_products = Product::where('name', 'LIKE', '%' . $this->main_search . '%')->orderBy('name', 'ASC')->get();
+        }
+        $this->cart_item_count = Cart::where('user_id', Auth::user()->id)->count();
+        $this->cartItems = Cart::where('user_id', Auth::user()->id)->get();
+        return view('livewire.user.contact-component', ['live_search_products' => $live_search_products]);
     }
 }
