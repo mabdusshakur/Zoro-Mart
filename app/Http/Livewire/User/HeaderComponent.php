3a0efab10c83ec89cc0feab6f411b1643ea199b8
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-16 22:28:54 +0600

    Header codes added

--- app/Http/Livewire/User/HeaderComponent.php
+++ app/Http/Livewire/User/HeaderComponent.php
@@ -2,10 +2,17 @@
 
 namespace App\Http\Livewire\User;
 
+use App\Models\Utility;
 use Livewire\Component;
 
 class HeaderComponent extends Component
 {
+    public $hotline;
+
+    public function mount()
+    {
+        $this->hotline = optional(Utility::first())->hotline;
+    }
     public function render()
     {
         return view('livewire.user.header-component');
