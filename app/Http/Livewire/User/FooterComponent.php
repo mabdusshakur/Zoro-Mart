671b93de318918aaa24132f39ebddfc3f2aa2191
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-16 22:38:58 +0600

    existing footer backend codes added

--- app/Http/Livewire/User/FooterComponent.php
+++ app/Http/Livewire/User/FooterComponent.php
@@ -3,9 +3,16 @@
 namespace App\Http\Livewire\User;
 
 use Livewire\Component;
+use App\Models\SocialMedia;
 
 class FooterComponent extends Component
 {
+    public $socialmedias;
+
+    public function mount()
+    {
+        $this->socialmedias = SocialMedia::all();
+    }
     public function render()
     {
         return view('livewire.user.footer-component');
