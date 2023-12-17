b22954e702e804733013ce8f649139352f614208
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-17 21:59:41 +0600

    mail search product image added

--- resources/views/livewire/user/search-component.blade.php
+++ resources/views/livewire/user/search-component.blade.php
@@ -5,15 +5,22 @@
         </div>
         <div class="col-md-8">
             <div class="card position-absolute">
-                @foreach ($live_search_products as $product)    
-                <div class="row align-items-center justify-content-center mt-2">
-                    <div class="col-md-4 col-sm-6">
-                        <img src="{{ asset('admin/assets/images/avatars/01.png') }}" alt="Product Image" width="40px" height="40px" class="rounded-circle">
+                @foreach ($live_search_products as $product)
+                    @php
+                    $images = App\Models\product_image::where('product_uid', $product->product_uid)->get()->take(1);
+                    @endphp
+                    <div class="row align-items-center justify-content-center mt-2">
+                        <div class="col-md-4 col-sm-6">
+                            <img src="{{ Storage::url($images[0]->image) }}" alt="Product Image" width="40px" height="40px"
+                                class="rounded-circle">
+                        </div>
+                        <div class="col-md-6 col-sm-6 mt-2">
+                            <a class="product-item-link" href="javascript:;"
+                                wire:click="viewProductDetails({{ $product->id }},'{{ $product->slug }}',{{ $product->category_id }},{{ $product->sub_category_id }})">
+                                <h5>{{ $product->name }}</h5>
+                            </a>
+                        </div>
                     </div>
-                    <div class="col-md-6 col-sm-6 mt-2">
-                        <a class="product-item-link" href="javascript:;" wire:click="viewProductDetails({{ $product->id }},'{{ $product->slug }}',{{ $product->category_id }},{{ $product->sub_category_id }})"><h5>{{$product->name}}</h5></a>
-                    </div>
-                </div>
                 @endforeach
             </div>
         </div>
