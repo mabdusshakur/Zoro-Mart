7fb60d95cc601f1924d659398e0de5ca7c822996
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-17 14:43:48 +0600

    subscribe newsletter added

--- app/Http/Livewire/User/NewsLetterComponent.php
+++ app/Http/Livewire/User/NewsLetterComponent.php
@@ -3,9 +3,23 @@
 namespace App\Http\Livewire\User;
 
 use Livewire\Component;
+use App\Models\NewsLetter;
 
 class NewsLetterComponent extends Component
 {
+    public $emial;
+    public function subscribeNewsLetter()
+    {
+        $this->validate([
+            'email' => 'required|email|unique:news_letters,email'
+        ]);
+        NewsLetter::creat([
+            'email' => $this->email,
+            'status' => 1
+        ]);
+        session()->flash('success', 'You have subscribed successfully!');
+        $this->reset('email');
+    }
     public function render()
     {
         return view('livewire.user.news-letter-component');
