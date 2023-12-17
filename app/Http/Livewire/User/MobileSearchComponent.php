d6d5a69eb108d2476e017b242e39bb5443cdd600
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-17 21:51:52 +0600

    mobile search component

---
+++ app/Http/Livewire/User/MobileSearchComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\User;
+
+use Livewire\Component;
+
+class MobileSearchComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.user.mobile-search-component');
+    }
+}
