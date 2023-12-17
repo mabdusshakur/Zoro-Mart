50e75a4e16404004e6176b4d89d5650c20019194
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-17 22:00:13 +0600

    Mobile search product image added

--- resources/views/livewire/user/mobile-search-component.blade.php
+++ resources/views/livewire/user/mobile-search-component.blade.php
@@ -3,9 +3,12 @@
         <input type="text" placeholder="What are you searching for?" wire:model="main_search">
     </form>
     @foreach ($live_search_products as $product)
+    @php
+    $images = App\Models\product_image::where('product_uid', $product->product_uid)->get()->take(1);
+    @endphp
         <div class="row align-items-center justify-content-center mt-2">
             <div class="col-2">
-                <img src="{{ asset('admin/assets/images/avatars/01.png') }}" alt="Product Image" width="30px"
+                <img src="{{ Storage::url($images[0]->image) }}" alt="Product Image" width="30px"
                     height="30px" class="rounded-circle">
             </div>
             <div class="col-10">
