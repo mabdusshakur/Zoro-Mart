68850ff2671a54b503b6911b8f41e8e16985a06f
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-17 22:48:39 +0600

    dynamic valeu retrieved

--- app/Http/Livewire/User/FooterComponent.php
+++ app/Http/Livewire/User/FooterComponent.php
@@ -2,16 +2,19 @@
 
 namespace App\Http\Livewire\User;
 
+use App\Models\Utility;
 use Livewire\Component;
 use App\Models\SocialMedia;
 
 class FooterComponent extends Component
 {
     public $socialmedias;
+    public $web_name;
 
     public function mount()
     {
         $this->socialmedias = SocialMedia::all();
+        $this->web_name = optional(Utility::first())->web_name;
     }
     public function render()
     {
