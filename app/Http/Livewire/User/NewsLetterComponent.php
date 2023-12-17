5913c29e275b46790c89662db155c8d5ca28d5a3
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-17 14:37:27 +0600

    newsLetter component

---
+++ app/Http/Livewire/User/NewsLetterComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\User;
+
+use Livewire\Component;
+
+class NewsLetterComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.user.news-letter-component');
+    }
+}
