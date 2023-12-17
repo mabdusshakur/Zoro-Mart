1ac288e124c3609548c471d63dce006e20263ee9
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-17 15:00:46 +0600

    checked for if user auth and if already the email is on the newsletter then newsletter component wont show

--- resources/views/livewire/user/news-letter-component.blade.php
+++ resources/views/livewire/user/news-letter-component.blade.php
@@ -1,28 +1,60 @@
 <div>
-    @include('partials.user._alerts')
-    <section class="subscribe__area pt-35 pb-30">
-        <div class="container">
-            <div class="row align-items-center">
-                <div class="col-xl-6 col-lg-6">
-                    <div class="subscribe__content d-sm-flex align-items-center">
-                        <div class="subscribe__icon mr-25">
-                            <img src="{{ asset('user/assets/img/icon/icon_email.png') }}" alt="" />
+    @if (Auth::check())
+        @if (!\App\Models\NewsLetter::where('email', Auth::user()->email)->exists())
+            @include('partials.user._alerts')
+            <section class="subscribe__area pt-35 pb-30">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-xl-6 col-lg-6">
+                            <div class="subscribe__content d-sm-flex align-items-center">
+                                <div class="subscribe__icon mr-25">
+                                    <img src="{{ asset('user/assets/img/icon/icon_email.png') }}" alt="" />
+                                </div>
+                                <div class="subscribe__text">
+                                    <h4>Sign up to Newsletter</h4>
+                                    <p>Get email updates about our latest shop...</p>
+                                </div>
+                            </div>
                         </div>
-                        <div class="subscribe__text">
-                            <h4>Sign up to Newsletter</h4>
-                            <p>Get email updates about our latest shop...</p>
+                        <div class="col-xl-6 col-lg-6">
+                            <div class="subscribe__form f-right">
+                                <form wire:submit.prevent="subscribeNewsLetter">
+                                    <input type="email" placeholder="Enter your email here..."
+                                        wire:model.lazy="email" />
+                                    <button class="t-y-btn t-y-btn-sub" type="submit">subscribe</button>
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-xl-6 col-lg-6">
-                    <div class="subscribe__form f-right">
-                        <form wire:submit.prevent="subscribeNewsLetter">
-                            <input type="email" placeholder="Enter your email here..." wire:model.lazy="email"/>
-                            <button class="t-y-btn t-y-btn-sub" type="submit">subscribe</button>
-                        </form>
+            </section>
+        @endif
+    @else
+        @include('partials.user._alerts')
+        <section class="subscribe__area pt-35 pb-30">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-xl-6 col-lg-6">
+                        <div class="subscribe__content d-sm-flex align-items-center">
+                            <div class="subscribe__icon mr-25">
+                                <img src="{{ asset('user/assets/img/icon/icon_email.png') }}" alt="" />
+                            </div>
+                            <div class="subscribe__text">
+                                <h4>Sign up to Newsletter</h4>
+                                <p>Get email updates about our latest shop...</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-6 col-lg-6">
+                        <div class="subscribe__form f-right">
+                            <form wire:submit.prevent="subscribeNewsLetter">
+                                <input type="email" placeholder="Enter your email here..." wire:model.lazy="email" />
+                                <button class="t-y-btn t-y-btn-sub" type="submit">subscribe</button>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </section>
+        </section>
+    @endif
 </div>
