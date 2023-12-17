6fe8a1ce3dd08acd3bb2eef1bc16d7a77a2158c5
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-17 15:20:11 +0600

    alerts added

--- resources/views/livewire/admin/send-news-letter-component.blade.php
+++ resources/views/livewire/admin/send-news-letter-component.blade.php
@@ -1,5 +1,6 @@
 <div>
     <div class="card">
+        @include('partials.admin._alerts')
         <div class="card-header">
             <h6 class="mb-0">Send Newsletters</h6>
         </div>
@@ -12,7 +13,7 @@
                 <div class="col-md-12">
                     <textarea class="form-control" cols="30" rows="10" placeholder="Write Your News Here" wire:model="news"></textarea>
                 </div>
-                <div class="col-md-12 mt-2">
+                <div class="col-md-12 mt-2" >
                     <button type="submit" wire:click="sendNews" class="btn btn-success">Send News</button>
                 </div>
             </div>
