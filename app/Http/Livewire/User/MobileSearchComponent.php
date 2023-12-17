e13b914af8151dc4405f0d72c9a561ae230e4607
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-17 21:55:50 +0600

    search functionality added

--- app/Http/Livewire/User/MobileSearchComponent.php
+++ app/Http/Livewire/User/MobileSearchComponent.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Livewire\User;
 
+use App\Models\Product;
 use Livewire\Component;
 
 class MobileSearchComponent extends Component
 {
+    public $main_search;
     public function render()
     {
-        return view('livewire.user.mobile-search-component');
+        $live_search_products = [];
+        if(strlen($this->main_search) > 3){
+            $live_search_products = Product::where('name', 'LIKE', '%'.$this->main_search.'%')->orderBy('name', 'ASC')->get();
+        }
+        return view('livewire.user.mobile-search-component', ['live_search_products' => $live_search_products]);
     }
 }
--- resources/views/livewire/user/mobile-search-component.blade.php
+++ resources/views/livewire/user/mobile-search-component.blade.php
@@ -1,14 +1,19 @@
 <div>
     <form action="#">
-        <input type="text" placeholder="What are you searching for?">
+        <input type="text" placeholder="What are you searching for?" wire:model="main_search">
         <button type="submit"><i class="far fa-search"></i></button>
     </form>
-    <div class="row align-items-center justify-content-center mt-2">
-        <div class="col-2">
-            <img src="{{ asset('admin/assets/images/avatars/01.png') }}" alt="Product Image" width="30px" height="30px" class="rounded-circle">
+    @foreach ($live_search_products as $product)
+        <div class="row align-items-center justify-content-center mt-2">
+            <div class="col-2">
+                <img src="{{ asset('admin/assets/images/avatars/01.png') }}" alt="Product Image" width="30px"
+                    height="30px" class="rounded-circle">
+            </div>
+            <div class="col-10">
+                <a class="product-item-link" href="javascript:;">
+                    <h5>{{$product->name}}</h5>
+                </a>
+            </div>
         </div>
-        <div class="col-10">
-            <a class="product-item-link" href="javascript:;"><h5></h5></a>
-        </div>
-    </div>
+    @endforeach
 </div>
