316e36db0a1e66fd9a11895cfddeb2664ab2bcc7
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-17 21:27:54 +0600

    order status update mail added

--- app/Http/Livewire/Admin/OrderDetailsComponent.php
+++ app/Http/Livewire/Admin/OrderDetailsComponent.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Livewire\Admin;
 
+use App\Mail\OrderProgressMail;
 use App\Models\Order;
 use Livewire\Component;
+use Illuminate\Support\Facades\Mail;
 
 class OrderDetailsComponent extends Component
 {
@@ -16,8 +18,21 @@ class OrderDetailsComponent extends Component
     public function updateStatus()
     {
         $this->order->status = $this->status;
-        $this->order->save();
-        session()->flash('success', 'Order status has been updated successfully!');
+        if ($this->order->save()) {
+            session()->flash('success', 'Order status has been updated successfully!');
+            $mail_data = [
+                'subject' => 'Order Status Updated',
+                'email' => $this->order->user->email,
+                'buyer_name' => $this->order->user->name,
+                'order_status' => $this->status,
+                'order_id' => $this->order->id,
+            ];
+            if (Mail::to($mail_data['email'])->send(new OrderProgressMail($mail_data))) {
+                session()->flash('success', 'Order status email has been sent successfully!');
+            } else {
+                session()->flash('error', 'Mail send failed!');
+            }
+        }
     }
     public function render()
     {
