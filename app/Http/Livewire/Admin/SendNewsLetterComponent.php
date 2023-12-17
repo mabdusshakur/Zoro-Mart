a37a4d8e08cf51f752f7eaba94e0e241cac1caae
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-17 15:13:05 +0600

    send news form added

--- app/Http/Livewire/Admin/SendNewsLetterComponent.php
+++ app/Http/Livewire/Admin/SendNewsLetterComponent.php
@@ -6,6 +6,12 @@ use Livewire\Component;
 
 class SendNewsLetterComponent extends Component
 {
+    public $subject, $news;
+
+    public function sendNews()
+    {
+        
+    }
     public function render()
     {
         return view('livewire.admin.send-news-letter-component')->layout('layouts.admin');
--- resources/views/livewire/admin/send-news-letter-component.blade.php
+++ resources/views/livewire/admin/send-news-letter-component.blade.php
@@ -1,3 +1,21 @@
 <div>
-    {{-- Be like water. --}}
+    <div class="card">
+        <div class="card-header">
+            <h6 class="mb-0">Send Newsletters</h6>
+        </div>
+        <div class="card-body">
+            <div class="row">
+                <div class="col-md-4">
+                    <input class="form-control form-control mb-3" type="text" placeholder="Subject"
+                        wire:model="subject">
+                </div>
+                <div class="col-md-12">
+                    <textarea class="form-control" cols="30" rows="10" placeholder="Write Your News Here" wire:model="news"></textarea>
+                </div>
+                <div class="col-md-12 mt-2">
+                    <button type="submit" wire:click="sendNews" class="btn btn-success">Send News</button>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
