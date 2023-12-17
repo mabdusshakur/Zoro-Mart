0647d85da9b7f13da980a03aa506259c731b4778
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-17 19:39:05 +0600

    order search added on order details

--- app/Http/Livewire/Admin/OrderDetailsComponent.php
+++ app/Http/Livewire/Admin/OrderDetailsComponent.php
@@ -2,10 +2,16 @@
 
 namespace App\Http\Livewire\Admin;
 
+use App\Models\Order;
 use Livewire\Component;
 
 class OrderDetailsComponent extends Component
 {
+    public $order;
+    public function mount($id)
+    {
+        $this->order = Order::find($id);
+    }
     public function render()
     {
         return view('livewire.admin.order-details-component');
