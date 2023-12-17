44ef810e9a1b49fab866bab0bb85377be751cb63
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-17 15:17:02 +0600

    foreach news subscribers send letter added

--- app/Http/Livewire/Admin/SendNewsLetterComponent.php
+++ app/Http/Livewire/Admin/SendNewsLetterComponent.php
@@ -3,6 +3,9 @@
 namespace App\Http\Livewire\Admin;
 
 use Livewire\Component;
+use App\Models\NewsLetter;
+use App\Mail\NewsLetterMail;
+use Illuminate\Support\Facades\Mail;
 
 class SendNewsLetterComponent extends Component
 {
@@ -10,7 +13,21 @@ class SendNewsLetterComponent extends Component
 
     public function sendNews()
     {
+        $this->validate([
+            'subject' => 'required',
+            'news' => 'required'
+        ]);
+        $subscribers = NewsLetter::where('status', 1)->get();
+        $mail_data = [
+            'subject' => $this->subject,
+            'news' => $this->news
+        ];
         
+        foreach ($subscribers as $subscriber) {
+            Mail::to($subscriber->email)->send(new NewsLetterMail($mail_data));
+        }
+        session()->flash('success', 'News letter sent successfully!');
+        $this->reset(['subject', 'news']);
     }
     public function render()
     {
