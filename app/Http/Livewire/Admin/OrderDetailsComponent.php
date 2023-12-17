863bcb5ac7ea1b474172dca0414ea7dc22ce8a8e
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-17 20:09:12 +0600

    status update code added

--- app/Http/Livewire/Admin/OrderDetailsComponent.php
+++ app/Http/Livewire/Admin/OrderDetailsComponent.php
@@ -8,9 +8,16 @@ use Livewire\Component;
 class OrderDetailsComponent extends Component
 {
     public $order;
+    public $status;
     public function mount($id)
     {
         $this->order = Order::find($id);
+    }
+    public function updateStatus()
+    {
+        $this->order->status = $this->status;
+        $this->order->save();
+        session()->flash('success', 'Order status has been updated successfully!');
     }
     public function render()
     {
