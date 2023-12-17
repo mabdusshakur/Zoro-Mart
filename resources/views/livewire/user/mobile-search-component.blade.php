f1c8cc21e01d4e539de65b3a2e6a825a1f9824d9
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-17 21:52:54 +0600

    search frontend added

--- resources/views/livewire/user/mobile-search-component.blade.php
+++ resources/views/livewire/user/mobile-search-component.blade.php
@@ -1,3 +1,14 @@
 <div>
-    {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}
+    <form action="#">
+        <input type="text" placeholder="What are you searching for?">
+        <button type="submit"><i class="far fa-search"></i></button>
+    </form>
+    <div class="row align-items-center justify-content-center mt-2">
+        <div class="col-2">
+            <img src="{{ asset('admin/assets/images/avatars/01.png') }}" alt="Product Image" width="30px" height="30px" class="rounded-circle">
+        </div>
+        <div class="col-10">
+            <a class="product-item-link" href="javascript:;"><h5></h5></a>
+        </div>
+    </div>
 </div>
