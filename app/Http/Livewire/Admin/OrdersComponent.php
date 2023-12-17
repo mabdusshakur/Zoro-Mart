e13c676e58f8e064bc33b1b7295505c1f20d2a75
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-17 19:07:52 +0600

    orders retrieved

--- app/Http/Livewire/Admin/OrdersComponent.php
+++ app/Http/Livewire/Admin/OrdersComponent.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Livewire\Admin;
 
+use App\Models\Order;
 use Livewire\Component;
 
 class OrdersComponent extends Component
 {
     public function render()
     {
-        return view('livewire.admin.orders-component')->layout('layouts.admin');
+        $orders = Order::all();
+        return view('livewire.admin.orders-component', ['orders'=>$orders])->layout('layouts.admin');
     }
 }
