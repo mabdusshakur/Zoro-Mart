11051faee9ca5912c7c82645e29c5f40252caf5f
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-17 15:15:32 +0600

    newsLetter mailer and template added

---
+++ app/Mail/NewsLetterMail.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class NewsLetterMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $mail_data;
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($mail_data)
+    {
+        $this->mail_data = $mail_data;
+    }
+    /**
+     * Get the message envelope.
+     *
+     * @return \Illuminate\Mail\Mailables\Envelope
+     */
+    public function envelope()
+    {
+        return new Envelope(
+            subject: $this->mail_data['subject'],
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     *
+     * @return \Illuminate\Mail\Mailables\Content
+     */
+    public function content()
+    {
+        return new Content(
+            view: 'emails.news-letter-mail',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array
+     */
+    public function attachments()
+    {
+        return [];
+    }
+}
