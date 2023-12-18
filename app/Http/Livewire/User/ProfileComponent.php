41b84c43552ba2d71bfc31eb7dc7a96c45a457bb
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-18 20:38:19 +0600

    save profile added

--- app/Http/Livewire/User/ProfileComponent.php
+++ app/Http/Livewire/User/ProfileComponent.php
@@ -13,6 +13,18 @@ class ProfileComponent extends Component
         $this->first_name = auth()->user()->first_name;
         $this->last_name = auth()->user()->last_name;
     }
+    public function saveProfile()
+    {
+        $this->validate([
+            'first_name' => 'required|min:3',
+            'last_name' => 'required|min:3',
+        ]);
+        $user = auth()->user();
+        $user->first_name = $this->first_name;
+        $user->last_name = $this->last_name;
+        $user->save();
+        session()->flash('success', 'Profile has been updated successfully!');
+    }
     public function render()
     {
         return view('livewire.user.profile-component');
