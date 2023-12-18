b919a0d58cb89c5055f7dbf103717b5fb5df2efd
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-18 20:52:22 +0600

    formated

--- resources/views/layouts/admin/partials/_top_header.blade.php
+++ resources/views/layouts/admin/partials/_top_header.blade.php
@@ -1,73 +1,71 @@
 <header class="top-header">
     <nav class="navbar navbar-expand gap-3">
-      <div class="toggle-icon">
-        <i class="fa-solid fa-bars"></i>
-      </div>
-     
-
-      <div class="top-navbar-right ms-auto">
-
-        <ul class="navbar-nav align-items-center">
-
-          <li class="nav-item dropdown dropdown-user-setting">
-            <a class="nav-link dropdown-toggle dropdown-toggle-nocaret" href="javascript:;" data-bs-toggle="dropdown">
-              <div class="user-setting">
-                <img src="{{ asset('admin/assets/images/avatars/06.png') }}" class="user-img" alt="">
-              </div>
-            </a>
-            <ul class="dropdown-menu dropdown-menu-end">
-              <li>
-                <a class="dropdown-item" href="javascript:;">
-                  <div class="d-flex flex-row align-items-center gap-2">
-                    <img src="{{ asset('admin/assets/images/avatars/06.png') }}" alt="" class="rounded-circle" width="54" height="54">
-                    <div class="">
-                      <h6 class="mb-0 dropdown-user-name">{{Auth::user()->name}}</h6>
-                      <small class="mb-0 dropdown-user-designation text-secondary">{{Auth::user()->role}}</small>
-                    </div>
-                  </div>
-                </a>
-              </li>
-              <li>
-                <hr class="dropdown-divider">
-              </li>
-              <li>
-                <a class="dropdown-item" href="javascript:;">
-                  <div class="d-flex align-items-center">
-                    <div class="">
-                      <i class="fa-regular fa-circle-user"></i>
-                    </div>
-                    <div class="ms-3"><span>Profile</span></div>
-                  </div>
-                </a>
-              </li>
-              <li>
-                <a class="dropdown-item" href="javascript:;">
-                  <div class="d-flex align-items-center">
-                    <div class="">
-                      <i class="fa-solid fa-gear"></i>
-                    </div>
-                    <div class="ms-3"><span>Setting</span></div>
-                  </div>
-                </a>
-              </li>
-              <li>
-                <hr class="dropdown-divider">
-              </li>
-              <li>
-                <a class="dropdown-item" href="{{ route('user.logout') }}">
-                  <div class="d-flex align-items-center">
-                    <div class="">
-                      <ion-icon name="log-out-outline"></ion-icon>
-                    </div>
-                    <i class="fa-solid fa-right-from-bracket"></i>
-                  </div>
-                </a>
-              </li>
+        <div class="toggle-icon">
+            <i class="fa-solid fa-bars"></i>
+        </div>
+        <div class="top-navbar-right ms-auto">
+            <ul class="navbar-nav align-items-center">
+                <li class="nav-item dropdown dropdown-user-setting">
+                    <a class="nav-link dropdown-toggle dropdown-toggle-nocaret" href="javascript:;"
+                        data-bs-toggle="dropdown">
+                        <div class="user-setting">
+                            <img src="{{ asset('admin/assets/images/avatars/06.png') }}" class="user-img"
+                                alt="">
+                        </div>
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-end">
+                        <li>
+                            <a class="dropdown-item" href="javascript:;">
+                                <div class="d-flex flex-row align-items-center gap-2">
+                                    <img src="{{ asset('admin/assets/images/avatars/06.png') }}" alt=""
+                                        class="rounded-circle" width="54" height="54">
+                                    <div class="">
+                                        <h6 class="mb-0 dropdown-user-name">{{ Auth::user()->name }}</h6>
+                                        <small
+                                            class="mb-0 dropdown-user-designation text-secondary">{{ Auth::user()->role }}</small>
+                                    </div>
+                                </div>
+                            </a>
+                        </li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="javascript:;">
+                                <div class="d-flex align-items-center">
+                                    <div class="">
+                                        <i class="fa-regular fa-circle-user"></i>
+                                    </div>
+                                    <div class="ms-3"><span>Profile</span></div>
+                                </div>
+                            </a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="javascript:;">
+                                <div class="d-flex align-items-center">
+                                    <div class="">
+                                        <i class="fa-solid fa-gear"></i>
+                                    </div>
+                                    <div class="ms-3"><span>Setting</span></div>
+                                </div>
+                            </a>
+                        </li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="{{ route('user.logout') }}">
+                                <div class="d-flex align-items-center">
+                                    <div class="">
+                                        <ion-icon name="log-out-outline"></ion-icon>
+                                    </div>
+                                    <i class="fa-solid fa-right-from-bracket"></i>
+                                </div>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
             </ul>
-          </li>
-
-        </ul>
-
-      </div>
+        </div>
     </nav>
-  </header>
+</header>
