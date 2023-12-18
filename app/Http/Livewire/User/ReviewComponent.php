d9ae42e50ed8db31710185beffc7d20294223192
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-18 16:24:18 +0600

    review code added

--- app/Http/Livewire/User/ReviewComponent.php
+++ app/Http/Livewire/User/ReviewComponent.php
@@ -2,17 +2,29 @@
 
 namespace App\Http\Livewire\User;
 
+use App\Models\Review;
 use App\Models\Product;
 use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
 
 class ReviewComponent extends Component
 {
     public $product;
-
+    public $review, $rating;
     public function mount($product_id)
     {
         $this->product = Product::find($product_id);
     }
+    public function addReview()
+    {
+        Review::create([
+            'rating' => $this->rating,
+            'review' => $this->review,
+            'user_id' => Auth::user()->id,
+            'product_id' => $this->product->id,
+        ]);
+    }
+    
 
     public function render()
     {
