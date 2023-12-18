886a5c1d6b83609441c274ef596118d0e56daaff
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-18 20:28:15 +0600

    checked if user bought the product or not

--- resources/views/livewire/user/review-component.blade.php
+++ resources/views/livewire/user/review-component.blade.php
@@ -1,34 +1,40 @@
 <div>
-    @if (!\App\Models\Review::where('user_id', Auth::user()->id)->where('product_id', $product->id)->exists())
-        @include('partials.user._alerts')
-        <form wire:submit.prevent="addReview">
-            <div class="review-input-box mb-15 d-flex align-items-start">
-                <h4 class="review-input-title">Your Rating</h4>
-                <div class="review-input">
-                    <div class="review-ratings mb-10">
-                        <div class="review-ratings-single d-flex align-items-center">
-                            <select class="form-select" wire:model.lazy="rating">
-                                <option value="1">1 Star</option>
-                                <option value="2">2 Star</option>
-                                <option value="3">3 Star</option>
-                                <option value="4">4 Star</option>
-                                <option value="5">5 Star</option>
-                            </select>
+    @if (Auth::check() &&
+            Auth::user()->orders()->whereHas('orderItems', function ($query) use ($product) {
+                    $query->where('product_id', $product->id);
+                })->exists())
+        @if (!\App\Models\Review::where('user_id', Auth::user()->id)->where('product_id', $product->id)->exists())
+            @include('partials.user._alerts')
+            <form wire:submit.prevent="addReview">
+                <div class="review-input-box mb-15 d-flex align-items-start">
+                    <h4 class="review-input-title">Your Rating</h4>
+                    <div class="review-input">
+                        <div class="review-ratings mb-10">
+                            <div class="review-ratings-single d-flex align-items-center">
+                                <select class="form-select" wire:model.lazy="rating">
+                                    <option value="1">1 Star</option>
+                                    <option value="2">2 Star</option>
+                                    <option value="3">3 Star</option>
+                                    <option value="4">4 Star</option>
+                                    <option value="5">5 Star</option>
+                                </select>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="review-input-box d-flex align-items-start">
-                <h4 class="review-input-title">Review</h4>
-                <div class="review-input">
-                    <textarea wire:model.lazy="review"></textarea>
+                <div class="review-input-box d-flex align-items-start">
+                    <h4 class="review-input-title">Review</h4>
+                    <div class="review-input">
+                        <textarea wire:model.lazy="review"></textarea>
+                    </div>
                 </div>
-            </div>
-            <div class="review-sub-btn">
-                <button type="submit" class="t-y-btn t-y-btn-grey">submit
-                    review</button>
-            </div>
-        </form>
+                <div class="review-sub-btn">
+                    <button type="submit" class="t-y-btn t-y-btn-grey">submit
+                        review</button>
+                </div>
+            </form>
+        @endif
+
     @endif
 
 </div>
