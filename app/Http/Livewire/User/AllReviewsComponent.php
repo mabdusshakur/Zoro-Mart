1dfd5e0c80df3bbb93c41a177be6e1fa8c4ab04a
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-18 16:44:15 +0600

    reviews code added

--- app/Http/Livewire/User/AllReviewsComponent.php
+++ app/Http/Livewire/User/AllReviewsComponent.php
@@ -2,10 +2,16 @@
 
 namespace App\Http\Livewire\User;
 
+use App\Models\Review;
 use Livewire\Component;
 
 class AllReviewsComponent extends Component
 {
+    public $reviews;
+    public function mount($product_id)
+    {
+        $this->reviews = Review::where('product_id', $product_id)->get();
+    } 
     public function render()
     {
         return view('livewire.user.all-reviews-component');
