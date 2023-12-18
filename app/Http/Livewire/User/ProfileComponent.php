40aee4f544ca61301fab87585bd2d2ec6105e991
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-18 20:37:22 +0600

    vars added with mounted current profile infos

--- app/Http/Livewire/User/ProfileComponent.php
+++ app/Http/Livewire/User/ProfileComponent.php
@@ -6,6 +6,13 @@ use Livewire\Component;
 
 class ProfileComponent extends Component
 {
+    public $first_name, $last_name, $current_password, $new_password, $confirm_password;
+
+    public function mount()
+    {
+        $this->first_name = auth()->user()->first_name;
+        $this->last_name = auth()->user()->last_name;
+    }
     public function render()
     {
         return view('livewire.user.profile-component');
