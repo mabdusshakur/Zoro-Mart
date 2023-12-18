d3719ce40fe4f35f8a557f63efa0c6413bd676bd
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-18 16:17:08 +0600

    seperate review component added

---
+++ app/Http/Livewire/User/ReviewComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\User;
+
+use Livewire\Component;
+
+class ReviewComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.user.review-component');
+    }
+}
