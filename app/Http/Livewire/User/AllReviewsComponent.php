1dedd6f2ceee218d9a6309a281d8118ee255f8d3
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-18 16:39:41 +0600

    seperate all reviews component added

---
+++ app/Http/Livewire/User/AllReviewsComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\User;
+
+use Livewire\Component;
+
+class AllReviewsComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.user.all-reviews-component');
+    }
+}
