8a88c4fc2807f551bbe6377ea54b0752f2963bcc
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-18 21:00:54 +0600

    update info added

--- app/Http/Livewire/Admin/ProfileComponent.php
+++ app/Http/Livewire/Admin/ProfileComponent.php
@@ -7,7 +7,25 @@ use Livewire\Component;
 class ProfileComponent extends Component
 {
     public $first_name, $last_name, $email, $current_password, $new_password, $confirm_password;
-    
+
+    public function updateInformation()
+    {
+        $this->validate([
+            'first_name' => 'required|min:3',
+            'last_name' => 'required|min:3',
+            'email' => 'required|email|unique:users,email,' . auth()->user()->id,
+        ]);
+        $user = auth()->user();
+        $user->first_name = $this->first_name;
+        $user->last_name = $this->last_name;
+        $user->email = $this->email;
+
+        if ($user->save()) {
+            session()->flash('success', 'Profile has been updated successfully!');
+        } else {
+            session()->flash('error', 'Something went wrong!');
+        }
+    }
     public function render()
     {
         return view('livewire.admin.profile-component')->layout('layouts.admin');
