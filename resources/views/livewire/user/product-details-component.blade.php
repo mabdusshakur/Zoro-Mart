1e6ed22dca4fcf59df8e1df655e3aff8b99a0eaa
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-18 16:39:26 +0600

    review component linked

--- resources/views/livewire/user/product-details-component.blade.php
+++ resources/views/livewire/user/product-details-component.blade.php
@@ -172,45 +172,7 @@
                                     <div class="col-xxl-6 col-xl-6 col-lg-6">
                                         <div class="review-wrapper">
                                             <h3 class="block-title">Customer Reviews</h3>
-
-                                            @foreach ($reviews as $review)
-                                                <div class="review-item">
-                                                    <h3 class="review-title">Nice</h3>
-                                                    <div class="review-ratings mb-10">
-                                                        <div class="review-ratings-single d-flex align-items-center">
-                                                            <span>Quality</span>
-                                                            <ul>
-                                                                @for ($i = 0; $i < 5; $i++)
-                                                                    @if ($i < $review->rating)
-                                                                        <li><a><i class="fa-solid fa-star"></i></a>
-                                                                        </li>
-                                                                    @else
-                                                                        <li>
-                                                                            <a>
-                                                                                <i class="fa-regular fa-star"></i>
-                                                                            </a>
-                                                                        </li>
-                                                                    @endif
-                                                                @endfor
-                                                            </ul>
-                                                        </div>
-                                                    </div>
-                                                    <div class="review-text">
-                                                        <p>{{ $review->review }}</p>
-                                                    </div>
-                                                    <div class="review-meta">
-                                                        <div class="review-author">
-                                                            <span>Review By : </span>
-                                                            <span>{{ $review->user->first_name }}</span>
-                                                        </div>
-                                                        <div class="review-date">
-                                                            <span>Posted on</span>
-                                                            <span>{{ $review->created_at->format('d-M-Y') }}</span>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            @endforeach
-
+                                            @livewire('user.all-reviews-component', ['product_id' => $product->id])
                                         </div>
                                     </div>
                                     <div class="col-xxl-4 col-xl-4 col-lg-4">
