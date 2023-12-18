ba15d79df8cd4017b093aff5598c30cde7f08355
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-18 16:20:47 +0600

    product id catched

--- app/Http/Livewire/User/ReviewComponent.php
+++ app/Http/Livewire/User/ReviewComponent.php
@@ -2,10 +2,18 @@
 
 namespace App\Http\Livewire\User;
 
+use App\Models\Product;
 use Livewire\Component;
 
 class ReviewComponent extends Component
 {
+    public $product;
+
+    public function mount($product_id)
+    {
+        $this->product = Product::find($product_id);
+    }
+
     public function render()
     {
         return view('livewire.user.review-component');
