d03a2b3da7c406a9949d1de039f4ebb0d73a17d2
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-18 20:59:58 +0600

    vars defined

--- app/Http/Livewire/Admin/ProfileComponent.php
+++ app/Http/Livewire/Admin/ProfileComponent.php
@@ -6,6 +6,8 @@ use Livewire\Component;
 
 class ProfileComponent extends Component
 {
+    public $first_name, $last_name, $email, $current_password, $new_password, $confirm_password;
+    
     public function render()
     {
         return view('livewire.admin.profile-component')->layout('layouts.admin');
