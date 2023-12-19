ed4a48ca38a5fba4bab09baa3df2caf96cd788d5
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-19 21:36:26 +0600

    removed wishList function

--- app/Http/Livewire/User/HomeComponent.php
+++ app/Http/Livewire/User/HomeComponent.php
@@ -30,19 +30,7 @@ class HomeComponent extends BaseComponent
         $this->features = Feature::take(5)->get();
         $this->socialmedias = SocialMedia::all();
     }
-    public function addToWishlist($id)
-    {
-        $product = Product::find($id);
-        if (Wishlist::where('user_id', Auth::user()->id)->where('product_id', $product->id)->first()) {
-            return redirect()->route('user.wishlist');
-        } else {
-            Wishlist::create([
-                'user_id' => Auth::user()->id,
-                'product_id' => $product->id,
-            ]);
-            return redirect()->route('user.wishlist');
-        }
-    }
+
     public function showProductModal($id)
     {
         $product = Product::find($id);
