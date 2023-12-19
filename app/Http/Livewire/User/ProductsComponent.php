fffc7996c9d4a8d36926a037146f2a4117d8bbca
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-19 21:59:49 +0600

    show product modal and add to wishlist function removed

--- app/Http/Livewire/User/ProductsComponent.php
+++ app/Http/Livewire/User/ProductsComponent.php
@@ -17,9 +17,6 @@ class ProductsComponent extends Component
     use WithPagination;
     public $per_page_item, $filter_item;
     public $minPrice, $maxPrice;
-
-    public $product_name, $product_description, $product_price, $product_quantity, $product_uid, $product_id;
-
     public $main_search;
 
     public $search_category_slug, $search_category_id, $search_sub_category_id;
@@ -40,29 +37,6 @@ class ProductsComponent extends Component
         $this->maxPrice = Product::max('price');
         $this->per_page_item = 10;
         $this->filter_item = 'by_name';
-    }
-    public function addToWishlist($id)
-    {
-        $product = Product::find($id);
-        if (Wishlist::where('user_id', Auth::user()->id)->where('product_id', $product->id)->first()) {
-            return redirect()->route('user.wishlist');
-        } else {
-            Wishlist::create([
-                'user_id' => Auth::user()->id,
-                'product_id' => $product->id,
-            ]);
-            return redirect()->route('user.wishlist');
-        }
-    }
-    public function showProductModal($id)
-    {
-        $product = Product::find($id);
-        $this->product_name = $product->name;
-        $this->product_description = $product->description;
-        $this->product_quantity = $product->quantity;
-        $this->product_price = $product->price;
-        $this->product_uid = $product->product_uid;
-        $this->product_id = $product->id;
     }
 
     public function addToCart($id)
