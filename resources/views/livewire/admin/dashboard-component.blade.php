b39ab600b6407d2be007e18afd942bbc9b67ce9f
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-20 00:03:15 +0600

    total user card added

--- resources/views/livewire/admin/dashboard-component.blade.php
+++ resources/views/livewire/admin/dashboard-component.blade.php
@@ -4,6 +4,23 @@
     @section('current-page-name', '')
     @include('partials.admin._bread_crumb')
     <!--end breadcrumb-->
-    
-    
+
+    <div class="row row-cols-1 row-cols-lg-2 row-cols-xl-4">
+        <div class="col">
+            <div class="card rounded-4">
+                <div class="card-body">
+                    <div class="d-flex align-items-center">
+                        <div class="">
+                            <p class="mb-1">Total Users</p>
+                            <h4 class="mb-0"></h4>
+                        </div>
+                        <div class="ms-auto widget-icon bg-primary text-white">
+                            <i class="bi bi-basket2"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </div>
