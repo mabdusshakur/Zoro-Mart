a4ddf24e0691e9429c1dcb613f0e8b711e1193e9
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-19 21:47:28 +0600

    showProductModal func.. added

--- app/Http/Livewire/User/BaseComponent.php
+++ app/Http/Livewire/User/BaseComponent.php
@@ -8,7 +8,9 @@ use App\Models\Wishlist;
 use Illuminate\Support\Facades\Auth;
 
 class BaseComponent extends Component
-{
+{    
+    public $product_name, $product_description, $product_price, $product_quantity, $product_uid, $product_id;
+   
     public function addToWishlist($id)
     {
         $product = Product::find($id);
@@ -26,5 +28,16 @@ class BaseComponent extends Component
     public function viewProductDetails($id, $slug, $category_id, $sub_category_id)
     {
         return redirect()->route('user.product-details', ['id' => $id, 'slug' => $slug, 'category_id' => $category_id, 'sub_category_id' => $sub_category_id]);
+    }
+
+    public function showProductModal($id)
+    {
+        $product = Product::find($id);
+        $this->product_name = $product->name;
+        $this->product_description = $product->description;
+        $this->product_quantity = $product->quantity;
+        $this->product_price = $product->price;
+        $this->product_uid = $product->product_uid;
+        $this->product_id = $product->id;
     }
 }
