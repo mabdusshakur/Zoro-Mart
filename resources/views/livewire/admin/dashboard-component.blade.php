018759bfe5a4fbe4ae7b86741b51eec0c52c5ecd
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-20 00:23:12 +0600

    number formating added

--- resources/views/livewire/admin/dashboard-component.blade.php
+++ resources/views/livewire/admin/dashboard-component.blade.php
@@ -12,7 +12,7 @@
                     <div class="d-flex align-items-center">
                         <div class="">
                             <p class="mb-1">Total Users</p>
-                            <h4 class="mb-0">{{$total_users}}</h4>
+                            <h4 class="mb-0">{{$total_users >= 1000 ? number_format($total_users / 1000, 1) . 'K' : $total_users }}</h4>
                         </div>
                         <div class="ms-auto widget-icon bg-primary text-white">
                             <i class="fa-solid fa-user"></i>
@@ -27,7 +27,7 @@
                     <div class="d-flex align-items-center">
                         <div class="">
                             <p class="mb-1">Total Products</p>
-                            <h4 class="mb-0">{{$total_products}}</h4>
+                            <h4 class="mb-0">{{$total_products >= 1000 ? number_format($total_products / 1000, 1) . 'K' : $total_products }}</h4>
                         </div>
                         <div class="ms-auto widget-icon bg-info text-white">
                             <i class="fa-solid fa-boxes-stacked"></i>
@@ -42,7 +42,7 @@
                     <div class="d-flex align-items-center">
                         <div class="">
                             <p class="mb-1">Total Orders</p>
-                            <h4 class="mb-0">{{$total_orders}}</h4>
+                            <h4 class="mb-0">{{ $total_orders >= 1000 ? number_format($total_orders / 1000, 1) . 'K' : $total_orders }}</h4>
                         </div>
                         <div class="ms-auto widget-icon bg-success text-white">
                             <i class="fa-solid fa-truck-fast"></i>
@@ -57,7 +57,7 @@
                     <div class="d-flex align-items-center">
                         <div class="">
                             <p class="mb-1">Total Sales</p>
-                            <h4 class="mb-0">{{$total_sales}}</h4>
+                            <h4 class="mb-0">{{ $total_sales >= 1000 ? number_format($total_sales / 1000, 1) . 'K' : $total_sales }}</h4>
                         </div>
                         <div class="ms-auto widget-icon bg-warning text-white">
                             <i class="fa-solid fa-dollar-sign"></i>
@@ -72,7 +72,7 @@
                     <div class="d-flex align-items-center">
                         <div class="">
                             <p class="mb-1">Total Categories</p>
-                            <h4 class="mb-0">{{$total_categories}}</h4>
+                            <h4 class="mb-0">{{ $total_categories >= 1000 ? number_format($total_categories / 1000, 1) . 'K' : $total_categories }}</h4>
                         </div>
                         <div class="ms-auto widget-icon bg-dark text-white">
                             <i class="fa-solid fa-table-columns"></i>
@@ -87,7 +87,7 @@
                     <div class="d-flex align-items-center">
                         <div class="">
                             <p class="mb-1">Total Sub-Categories</p>
-                            <h4 class="mb-0">{{$total_subcategories}}</h4>
+                            <h4 class="mb-0">{{ $total_subcategories >= 1000 ? number_format($total_subcategories / 1000, 1) . 'K' : $total_subcategories }}</h4>
                         </div>
                         <div class="ms-auto widget-icon bg-danger text-white">
                             <i class="fa-solid fa-table-cells"></i>
