db524073c9f7cedd14d301bbf6e4ee840f8ea70a
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-19 21:36:37 +0600

    added wishList function

--- app/Http/Livewire/User/BaseComponent.php
+++ app/Http/Livewire/User/BaseComponent.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Livewire\User;
 
+use App\Models\Product;
 use Livewire\Component;
+use App\Models\Wishlist;
+use Illuminate\Support\Facades\Auth;
 
 class BaseComponent extends Component
 {
-    
+    public function addToWishlist($id)
+    {
+        $product = Product::find($id);
+        if (Wishlist::where('user_id', Auth::user()->id)->where('product_id', $product->id)->first()) {
+            return redirect()->route('user.wishlist');
+        } else {
+            Wishlist::create([
+                'user_id' => Auth::user()->id,
+                'product_id' => $product->id,
+            ]);
+            return redirect()->route('user.wishlist');
+        }
+    }
 }
