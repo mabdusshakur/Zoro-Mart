7f36edad69e8f73e8e9a147ee5a97c2513990d37
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-20 00:09:29 +0600

    dynamic values assigned

--- resources/views/livewire/admin/dashboard-component.blade.php
+++ resources/views/livewire/admin/dashboard-component.blade.php
@@ -12,7 +12,7 @@
                     <div class="d-flex align-items-center">
                         <div class="">
                             <p class="mb-1">Total Users</p>
-                            <h4 class="mb-0"></h4>
+                            <h4 class="mb-0">{{$total_user}}</h4>
                         </div>
                         <div class="ms-auto widget-icon bg-primary text-white">
                             <i class="bi bi-basket2"></i>
@@ -27,7 +27,7 @@
                     <div class="d-flex align-items-center">
                         <div class="">
                             <p class="mb-1">Total Products</p>
-                            <h4 class="mb-0"></h4>
+                            <h4 class="mb-0">{{$total_products}}</h4>
                         </div>
                         <div class="ms-auto widget-icon bg-info text-white">
                             <i class="bi bi-basket2"></i>
@@ -42,7 +42,7 @@
                     <div class="d-flex align-items-center">
                         <div class="">
                             <p class="mb-1">Total Orders</p>
-                            <h4 class="mb-0"></h4>
+                            <h4 class="mb-0">{{$total_orders}}</h4>
                         </div>
                         <div class="ms-auto widget-icon bg-success text-white">
                             <i class="bi bi-basket2"></i>
