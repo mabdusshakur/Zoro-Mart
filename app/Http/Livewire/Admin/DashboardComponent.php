639b4a1c90cdac8d77a0f89c53fcf58a30bb4bb4
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-20 00:08:40 +0600

    user, order, product count added

--- app/Http/Livewire/Admin/DashboardComponent.php
+++ app/Http/Livewire/Admin/DashboardComponent.php
@@ -2,11 +2,21 @@
 
 namespace App\Http\Livewire\Admin;
 
+use App\Models\User;
+use App\Models\Order;
+use App\Models\Product;
 use Livewire\Component;
 
 class DashboardComponent extends Component
 {
     public $total_user, $total_orders, $total_products, $total_categories, $total_subcategories, $total_sales;
+
+    public function mount()
+    {
+        $this->total_user = User::count();
+        $this->total_orders = Order::count();
+        $this->total_products = Product::count();
+    }
     public function render()
     {
         return view('livewire.admin.dashboard-component')->layout('layouts.admin');
