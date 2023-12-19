9fa381b0b27d63d7c97b9f079b1e941c713d150e
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-20 00:05:37 +0600

    necessary function added

--- app/Http/Livewire/Admin/DashboardComponent.php
+++ app/Http/Livewire/Admin/DashboardComponent.php
@@ -6,6 +6,7 @@ use Livewire\Component;
 
 class DashboardComponent extends Component
 {
+    public $total_user, $total_orders, $total_products, $total_categories, $total_subcategories, $total_sales;
     public function render()
     {
         return view('livewire.admin.dashboard-component')->layout('layouts.admin');
