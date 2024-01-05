44fb1238d26915195a047033f1a8bcdf621cba47
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-01-05 16:36:03 +0600

    class cleanup

--- app/Http/Livewire/Admin/AddProductComponent.php
+++ app/Http/Livewire/Admin/AddProductComponent.php
@@ -15,7 +15,7 @@ class AddProductComponent extends Component
 {
     use WithFileUploads;
 
-    public $product_name, $description, $price, $quantity, $product_image, $category_id, $sub_category_id, $categories, $sub_categories , $category_name, $sub_category_name, $on_sale, $discount_price;
+    public $product_name, $description, $price, $quantity, $product_image, $category_id, $sub_category_id, $categories, $sub_categories, $category_name, $sub_category_name, $on_sale, $discount_price;
     public $test_product_images;
     public function mount()
     {
@@ -36,8 +36,8 @@ class AddProductComponent extends Component
                 'discount_price' => 'nullable|numeric',
                 'on_sale' => 'nullable|boolean',
             ]);
-            
-            
+
+
             $product_uid = Carbon::now()->timestamp . uniqid();
 
             $product = new Product();
@@ -53,14 +53,14 @@ class AddProductComponent extends Component
             $product->discount_price = $this->discount_price;
 
             foreach ($this->product_image as $image) {
-                $image_location = $image->storeAs('product/images', rand(1000,10000).'-'.time().'.'.$image->getClientOriginalExtension(), 'public');
+                $image_location = $image->storeAs('product/images', rand(1000, 10000) . '-' . time() . '.' . $image->getClientOriginalExtension(), 'public');
                 $product_image = new product_image();
                 $product_image->image = $image_location;
                 $product_image->product_uid = $product_uid;
                 $product_image->save();
             }
-            Category::find($this->category_id)->increment('product_count',1);
-            SubCategory::find($this->sub_category_id)->increment('product_count',1);
+            Category::find($this->category_id)->increment('product_count', 1);
+            SubCategory::find($this->sub_category_id)->increment('product_count', 1);
             $product->save();
             session()->flash('success', 'Product has been added successfully!');
         } catch (\Throwable $th) {
@@ -69,10 +69,10 @@ class AddProductComponent extends Component
     }
     public function render()
     {
-        if($this->category_id != null){
+        if ($this->category_id != null) {
             $this->category_name = Category::where('id', $this->category_id)->first()->name;
         }
-        if($this->sub_category_id != null){
+        if ($this->sub_category_id != null) {
             $this->sub_category_name = SubCategory::where('id', $this->sub_category_id)->first()->name;
         }
         return view('livewire.admin.add-product-component')->layout('layouts.admin');
