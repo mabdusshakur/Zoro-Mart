e8209324d803bb24a5dbecd14324594d69ed19b1
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-01-08 17:31:34 +0600

    c cleanup 2

--- app/Http/Livewire/Admin/SocialMediaComponent.php
+++ app/Http/Livewire/Admin/SocialMediaComponent.php
@@ -7,13 +7,9 @@ use App\Models\SocialMedia;
 
 class SocialMediaComponent extends Component
 {
-    public $name,$icon,$link;
-    public $update_name,$update_icon,$update_link;
+    public $name, $icon, $link;
+    public $update_name, $update_icon, $update_link;
 
-    public function mount()
-    {
-
-    }
     public function addSocial()
     {
         $this->validate([
@@ -25,7 +21,7 @@ class SocialMediaComponent extends Component
         $social->icon = $this->icon;
         $social->link = $this->link;
         $social->save();
-        session()->flash('success','Social Media has been created successfully!');
+        session()->flash('success', 'Social Media has been created successfully!');
     }
     public function saveSocial($id)
     {
@@ -33,30 +29,30 @@ class SocialMediaComponent extends Component
             'name' => 'required',
             'icon' => 'required',
         ]);
-        $social = SocialMedia::where('id',$id);
-        if($social){
+        $social = SocialMedia::where('id', $id);
+        if ($social) {
             $social->name = $this->name;
             $social->icon = $this->icon;
             $social->link = $this->link;
             $social->save();
-        }else{
-            session()->flash('warning','Social Media Not Found!');
+        } else {
+            session()->flash('warning', 'Social Media Not Found!');
         }
-        session()->flash('success','Social Media has been saved successfully!');
+        session()->flash('success', 'Social Media has been saved successfully!');
     }
     public function deleteSocial($id)
     {
-        $social = SocialMedia::where('id',$id);
-        if($social){
+        $social = SocialMedia::where('id', $id);
+        if ($social) {
             $social->delete();
-        }else{
-            session()->flash('warning','Social Media Not Found!');
+        } else {
+            session()->flash('warning', 'Social Media Not Found!');
         }
-        session()->flash('success','Social Media has been deleted successfully!');
+        session()->flash('success', 'Social Media has been deleted successfully!');
     }
     public function render()
     {
         $social_medias = SocialMedia::all();
-        return view('livewire.admin.social-media-component',['social_medias' => $social_medias])->layout('layouts.admin');
+        return view('livewire.admin.social-media-component', ['social_medias' => $social_medias])->layout('layouts.admin');
     }
 }
